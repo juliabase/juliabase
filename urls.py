cbbaf13b7d82b0bc82da2d014b1a30c14356e5a3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-01 15:57:04 +0200

    Added provisionary static media path (necessary for internal web server).

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^chantal/', include('chantal.foo.urls')),
+#                       (r'^samples/edit/(?P<sample_name>.+)', 'samples.views.edit_sample'),
+                       (r'^samples/(?P<sample_name>.+)', 'samples.views.show_sample'),
+#                       (r'^processes/edit/(?P<process_name>.+)', 'samples.views.edit_process'),
+                       (r'^admin/', include('django.contrib.admin.urls')),
+                       )
 
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
-)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            (r'^static_media/(?P<path>.*)$', 'django.views.static.serve',
+                             {'document_root': '/home/bronger/src/chantal/media/'}),
+                            )
