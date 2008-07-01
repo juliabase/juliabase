455af128c7dd67e434cb621ccf3bf8341716a03e
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-01 15:54:44 +0200

    Rudimetary version.

--- samples/views.py
+++ samples/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+import models
+
+def show_sample(request, sample_name):
+    sample = get_object_or_404(models.Sample, pk=sample_name)
+    processes = []
+    for process in sample.processes.all():
+        processes.append({"timestamp": process.timestamp, "title": process.__class__.__name__})
+    return render_to_response("show_sample.html", {"name": sample.name, "processes": processes})
