025706b1103bb8acecc00a4d9c39f68187f65bb8
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-03 06:01:16 +0200

    Made everything more sane: Actuall process data is now completely extracted from the object inself, instead of using a tuple of typles.

--- samples/views.py
+++ samples/views.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import string
 from django.http import HttpResponse
+from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 import models
+
+def camel_case_to_underscores(name):
+    result = []
+    for i, character in enumerate(name):
+        if i == 0:
+            result.append(character.lower())
+        elif character in string.ascii_uppercase:
+            result.extend(("_", character.lower()))
+        else:
+            result.append(character)
+    return "".join(result)
+
+def digest_process(process):
+    process = models.find_actual_process(process)
+    template = loader.get_template("show_"+camel_case_to_underscores(process.__class__.__name__)+".html")
+    return process, process._meta.verbose_name, template.render(Context({"process": process}))
 
 def show_sample(request, sample_name):
     sample = get_object_or_404(models.Sample, pk=sample_name)
     processes = []
     for process in sample.processes.all():
-        processes.append({"timestamp": process.timestamp, "title": process.__class__.__name__})
+        process, title, body = digest_process(process)
+        processes.append({"timestamp": process.timestamp, "title": title, "operator": process.operator,
+                          "body": body})
     return render_to_response("show_sample.html", {"name": sample.name, "processes": processes})
