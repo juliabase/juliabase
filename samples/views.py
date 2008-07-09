47015edc36cd11cc2b6899be1c76d7b6394df78c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-09 14:09:11 +0200

    Added rudimentary 6-chamber edit view.  Doesn't work yet.

--- samples/views.py
+++ samples/views.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import string
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
+from django.newforms import ModelForm
+import django.newforms as forms
 import models
 
 def camel_case_to_underscores(name):
@@ -31,3 +33,70 @@ def show_sample(request, sample_name):
         processes.append({"timestamp": process.timestamp, "title": title, "operator": process.operator,
                           "body": body})
     return render_to_response("show_sample.html", {"name": sample.name, "processes": processes})
+
+class SixChamberDepositionForm(ModelForm):
+    class Meta:
+        model = models.SixChamberDeposition
+
+class SixChamberLayerForm(ModelForm):
+    def __init__(self, **keyw):
+        super(SixChamberLayerForm, self).__init__(**keyw)
+        self.fields["number"].widget = \
+            forms.TextInput(attrs={"size": "2", "style": "text-align: center; font-size: xx-large"})
+        self.fields["chamber"].widget = forms.TextInput(attrs={"size": "3"})
+        self.fields["comments"].widget = forms.Textarea(attrs={"cols": "30"})
+        for fieldname in ["pressure", "time", "substrate_electrode_distance", "transfer_in_chamber", "pre_heat",
+                          "argon_pre_heat", "heating_temperature", "transfer_out_of_chamber", "plasma_start_power",
+                          "deposition_frequency", "deposition_power", "base_pressure"]:
+            self.fields[fieldname].widget = forms.TextInput(attrs={"size": "10"})
+    class Meta:
+        model = models.SixChamberLayer
+
+class SixChamberChannelForm(ModelForm):
+    def __init__(self, **keyw):
+        super(SixChamberChannelForm, self).__init__(**keyw)
+        self.fields["number"].widget = forms.TextInput(attrs={"size": "3", "style": "text-align: center"})
+        self.fields["gas"].widget = forms.TextInput(attrs={"size": "10"})
+        self.fields["diluted_in"].widget = forms.TextInput(attrs={"size": "10"})
+        self.fields["concentration"].widget = forms.TextInput(attrs={"size": "5"})
+        self.fields["flow_rate"].widget = forms.TextInput(attrs={"size": "7"})
+    class Meta:
+        model = models.SixChamberChannel
+
+def edit_six_chamber_deposition(request, deposition_number):
+    def is_all_valid(deposition_form, layer_forms, channel_forms):
+        if not deposition_form.is_valid():
+            return False
+        if any([not layer_form.is_valid() for layer_form in layer_forms]):
+            return False
+        for forms in channel_forms:
+            if any([not channel_form.is_valid() for channel_form in forms]):
+                return False
+        return True
+    
+    six_chamber_deposition = get_object_or_404(models.SixChamberDeposition, deposition_number=deposition_number)
+    if request.method == "POST":
+        deposition_form = SixChamberDepositionForm(request.POST, instance=six_chamber_deposition)
+        layer_forms = [SixChamberLayerForm(request.POST, prefix=str(layer.number), instance=layer)
+                       for layer in six_chamber_deposition.sixchamberlayer_set.all()]
+        channel_forms = []
+        for layer in six_chamber_deposition.sixchamberlayer_set.all():
+            channel_forms.append(
+                [SixChamberChannelForm(request.POST, prefix=str(layer.number)+"_"+str(channel.number), instance=channel)
+                 for channel in layer.sixchamberchannel_set.all()])
+        if is_all_valid(deposition_form, layer_forms, channel_forms):
+            # Todo: Write data back into the database
+            return HttpResponseRedirect("/admin")
+    else:
+        deposition_form = SixChamberDepositionForm(instance=six_chamber_deposition)
+        layer_forms = [SixChamberLayerForm(prefix=str(layer.number), instance=layer)
+                       for layer in six_chamber_deposition.sixchamberlayer_set.all()]
+        channel_forms = []
+        for layer in six_chamber_deposition.sixchamberlayer_set.all():
+            channel_forms.append(
+                [SixChamberChannelForm(prefix=str(layer.number)+"_"+str(channel.number), instance=channel)
+                 for channel in layer.sixchamberchannel_set.all()])
+    return render_to_response("edit_six_chamber_deposition.html",
+                              {"title": deposition_number,
+                               "deposition": deposition_form,
+                               "layers_and_channels": zip(layer_forms, channel_forms)})
--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 #                       (r'^samples/edit/(?P<sample_name>.+)', 'samples.views.edit_sample'),
                        (r'^samples/(?P<sample_name>.+)', 'samples.views.show_sample'),
-#                       (r'^processes/edit/(?P<process_name>.+)', 'samples.views.edit_process'),
+                       (r'^edit/6-chamber_deposition/(?P<deposition_number>.+)', 'samples.views.edit_six_chamber_deposition'),
                        (r'^admin/', include('django.contrib.admin.urls')),
                        )
 
