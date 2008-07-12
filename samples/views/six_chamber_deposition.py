70b63fb1dde33dfdff232cf913017d5392caf13c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-12 14:51:17 +0200

    Moved views in own package.

--- samples/views/six_chamber_deposition.py
+++ samples/views/six_chamber_deposition.py
@@ -1,39 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import string, re
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import Context, loader
-from django.shortcuts import render_to_response, get_object_or_404
-from django.newforms import ModelForm
-import django.newforms as forms
-import models
-
-def camel_case_to_underscores(name):
-    result = []
-    for i, character in enumerate(name):
-        if i == 0:
-            result.append(character.lower())
-        elif character in string.ascii_uppercase:
-            result.extend(("_", character.lower()))
-        else:
-            result.append(character)
-    return "".join(result)
-
-def digest_process(process):
-    process = models.find_actual_process(process)
-    template = loader.get_template("show_"+camel_case_to_underscores(process.__class__.__name__)+".html")
-    return process, process._meta.verbose_name, template.render(Context({"process": process}))
-
-def show_sample(request, sample_name):
-    sample = get_object_or_404(models.Sample, pk=sample_name)
-    processes = []
-    for process in sample.processes.all():
-        process, title, body = digest_process(process)
-        processes.append({"timestamp": process.timestamp, "title": title, "operator": process.operator,
-                          "body": body})
-    return render_to_response("show_sample.html", {"name": sample.name, "processes": processes})
-
 class SixChamberDepositionForm(ModelForm):
     class Meta:
         model = models.SixChamberDeposition
@@ -97,14 +64,14 @@ def edit_six_chamber_deposition(request, deposition_number):
                 layer_data = prefix_dict(layer_data, str(layer_index))
                 new_layers.append(SixChamberLayerForm(layer_data, prefix=str(layer_index)))
                 new_channel_lists.append(
-                        [SixChamberChannelForm(prefix_dict(channel.cleaned_data, str(layer_index)+"_"+str(channel_index)),
-                                               prefix=str(layer_index)+"_"+str(channel_index))
+                        [SixChamberChannelForm(prefix_dict(channel.cleaned_data, "%d_%d"%(layer_index, channel_index)),
+                                               prefix="%d_%d"%(layer_index, channel_index))
                          for channel_index, channel in enumerate(channel_forms[i])])
         # Second step: Add layers
         to_be_added_layers = int_or_zero(change_params["structural-change-add-layers"])
-        structure_changed = structure_changed or to_be_added_layers > 0
+        structure_changed = structure_changed or to_be_added_layers
         for i in range(to_be_added_layers):
-            layer_index = len(layer_forms)+len(new_layers)
+            layer_index = len(layer_forms) + len(new_layers)
             new_layers.append(SixChamberLayerForm({"%d-number"%layer_index: biggest_layer_number+1},
                                                   prefix=str(layer_index)))
             biggest_layer_number += 1
@@ -113,7 +80,7 @@ def edit_six_chamber_deposition(request, deposition_number):
             # Third step: Add channels
             to_be_added_channels = int_or_zero(change_params.get(
                     "structural-change-add-channels-for-layerindex-%d" % layer_index))
-            structure_changed = structure_changed or to_be_added_channels > 0
+            structure_changed = structure_changed or to_be_added_channels
             number_of_channels = len(channels)
             for channel_index in range(number_of_channels, number_of_channels+to_be_added_channels):
                 channels.append(SixChamberChannelForm(prefix="%d_%d"%(layer_index, channel_index)))
@@ -121,13 +88,13 @@ def edit_six_chamber_deposition(request, deposition_number):
             to_be_deleted_channels = [channel_index for channel_index in range(number_of_channels)
                                       if "structural-change-delete-channelindex-%d-for-layerindex-%d" %
                                       (channel_index, layer_index) in change_params]
-            structure_changed = structure_changed or to_be_deleted_channels > 0
+            structure_changed = structure_changed or to_be_deleted_channels
             for channel_index in reversed(to_be_deleted_channels):
                 del channels[channel_index]
         # Fifth step: Delete layers
         to_be_deleted_layers = [layer_index for layer_index in range(len(layer_forms))
                                 if "structural-change-delete-layerindex-%d" % layer_index in change_params]
-        structure_changed = structure_changed or to_be_deleted_layers > 0
+        structure_changed = structure_changed or to_be_deleted_layers
         for layer_index in reversed(to_be_deleted_layers):
             del layer_forms[layer_index]
         
@@ -154,14 +121,23 @@ def edit_six_chamber_deposition(request, deposition_number):
         channel_forms = []
         for layer_index in layer_indices:
             channel_forms.append(
-                [SixChamberChannelForm(request.POST, prefix=str(layer_index)+"_"+str(channel_index))
+                [SixChamberChannelForm(request.POST, prefix="%d_%d"%(layer_index, channel_index))
                  for channel_index in channels_indices[layer_index]])
 
         change_params = dict([(key, request.POST[key]) for key in request.POST if key.startswith("structural-change-")])
         all_valid = is_all_valid(deposition_form, layer_forms, channel_forms)
         structure_changed = change_structure(layer_forms, channel_forms, change_params)
         if all_valid and not structure_changed:
-            # Todo: Write data back into the database
+            deposition = deposition_form.save()
+            six_chamber_deposition.sixchamberlayer_set.all().delete()  # deletes channels, too
+            for layer_form, channel_form_list in zip(layer_forms, channel_forms):
+                layer = layer_form.save(commit=False)
+                layer.deposition = deposition
+                layer.save()
+                for channel_form in channel_form_list:
+                    channel = channel_form.save(commit=False)
+                    channel.layer = layer
+                    channel.save()
             return HttpResponseRedirect("/admin")
     else:
         deposition_form = SixChamberDepositionForm(instance=six_chamber_deposition)
@@ -171,7 +147,7 @@ def edit_six_chamber_deposition(request, deposition_number):
         channel_forms = []
         for layer_index, layer in enumerate(layers):
             channel_forms.append(
-                [SixChamberChannelForm(prefix=str(layer_index)+"_"+str(channel_index), instance=channel)
+                [SixChamberChannelForm(prefix="%d_%d"%(layer_index, channel_index), instance=channel)
                  for channel_index, channel in enumerate(layer.sixchamberchannel_set.all())])
     return render_to_response("edit_six_chamber_deposition.html",
                               {"title": deposition_number,
