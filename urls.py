fa5bbea729dcfc0804d21b12afb4f77b1f218172
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-15 22:09:05 +0200

    Added login page.

--- urls.py
+++ urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-urlpatterns = patterns('',
-#                       (r'^samples/edit/(?P<sample_name>.+)', 'samples.views.edit_sample'),
-                       (r'^samples/(?P<sample_name>.+)', 'samples.views.sample.show'),
-                       (r'^edit/6-chamber_deposition/(?P<deposition_number>.+)',
-                        'samples.views.six_chamber_deposition.edit'),
-                       (r'^admin/', include('django.contrib.admin.urls')),
+urlpatterns = patterns("",
+#                       (r"^samples/edit/(?P<sample_name>.+)", "samples.views.edit_sample"),
+                       (r"^samples/(?P<sample_name>.+)", "samples.views.sample.show"),
+                       (r"^edit/6-chamber_deposition/(?P<deposition_number>.+)",
+                        "samples.views.six_chamber_deposition.edit"),
+                       (r"^login/$", "django.contrib.auth.views.login", {"template_name": "login.html"}),
+                       (r"^admin/", include("django.contrib.admin.urls")),
                        )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-                            (r'^static_media/(?P<path>.*)$', 'django.views.static.serve',
-                             {'document_root': '/home/bronger/src/chantal/media/'}),
+    urlpatterns += patterns("",
+                            (r"^static_media/(?P<path>.*)$", "django.views.static.serve",
+                             {"document_root": "/home/bronger/src/chantal/media/"}),
                             )
