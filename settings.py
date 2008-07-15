caefdae206f47aeaa9bc16849b324f473fce4139
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-15 22:09:26 +0200

    Removed "sites" middleware.

--- settings.py
+++ settings.py
@@ -78,7 +78,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.admin',
     'chantal.samples'
 )
