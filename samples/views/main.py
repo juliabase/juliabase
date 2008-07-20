ec9bd109e3508b8f8a60317f624c4f90079e1e33
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-20 11:44:25 +0200

    Added about page.

--- samples/views/main.py
+++ samples/views/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import string
+import string, time
 from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from chantal.samples.models import Sample
@@ -13,6 +13,9 @@ from django.contrib.sites.models import Site
 from django import oldforms
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ungettext
+import django
+from django.conf import settings
 
 @login_required
 def main_menu(request):
@@ -58,3 +61,47 @@ def login(request, redirect_field_name=REDIRECT_FIELD_NAME):
         redirect_field_name: redirect_to,
         'site_name': current_site.name,
     }, context_instance=RequestContext(request))
+
+def breakup_time(seconds):
+    def test_timeunit(seconds, size_of_timeunit_in_seconds, translation_function, current_timeunit_list):
+        size_of_timeunit_in_seconds = int(round(size_of_timeunit_in_seconds))
+        number_of_timeunits = seconds // size_of_timeunit_in_seconds
+        if number_of_timeunits:
+            current_timeunit_list.append(translation_function(number_of_timeunits) % {"count": number_of_timeunits })
+        return number_of_timeunits * size_of_timeunit_in_seconds
+    current_timeunit_list = []
+    seconds = int(round(seconds))
+
+    chunks = ((365.2425*24*3600, lambda n: ungettext("%(count)d year", "%(count)d years", n)),
+              (30.436875*24*3600, lambda n: ungettext("%(count)d month", "%(count)d months", n)),
+              (7*24*3600, lambda n: ungettext("%(count)d week", "%(count)d weeks", n)),
+              (24*3600, lambda n: ungettext("%(count)d day", "%(count)d days", n)),
+              (3600, lambda n: ungettext("%(count)d hour", "%(count)d hours", n)),
+              (60, lambda n: ungettext("%(count)d minute", "%(count)d minutes", n)),
+              (1, lambda n: ungettext("%(count)d second", "%(count)d seconds", n)),
+              )
+    for duration, translation_function in chunks:
+        seconds -= test_timeunit(seconds, duration, translation_function, current_timeunit_list)
+    assert not seconds
+    if not current_timeunit_list:
+        current_timeunit_list = [ungettext("%(count)d second", "%(count)d seconds", 0) % {"count": 0 }]
+    if len(current_timeunit_list) == 1:
+        return current_timeunit_list[0]
+    else:
+        _ = ugettext
+        return _(", ").join(current_timeunit_list[:-1]) + _(", and ") + current_timeunit_list[-1]
+
+def about(request):
+    web_server_uptime = \
+        _("up and running for %(time)s") % { "time": breakup_time(time.time()-settings.APACHE_STARTUP_TIME)}
+    os_uptime = float(open("/proc/uptime").read().split()[0])
+    os_uptime = _("up and running for %(time)s") % { "time": breakup_time(os_uptime)}
+    return render_to_response("about.html", {"title": _(u"Chantal is presented to you by …"),
+                                             "os_uptime": os_uptime,
+                                             "web_server_version": settings.APACHE_VERSION,
+                                             "web_server_uptime": web_server_uptime,
+                                             "db_version": settings.MYSQL_VERSION,
+                                             "language_version": settings.PYTHON_VERSION,
+                                             "framework_version": django.get_version(),
+                                             },
+                              context_instance=RequestContext(request))
