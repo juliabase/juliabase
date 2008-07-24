a92a1756886787fbec47c3064a31794de8001b81
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-24 16:33:34 +0200

    Re-introduced excludes in model forms.

--- chantal/samples/views/six_chamber_deposition.py
+++ chantal/samples/views/six_chamber_deposition.py
@@ -56,6 +56,7 @@ class DepositionForm(ModelForm):
         return deposition
     class Meta:
         model = SixChamberDeposition
+        exclude = ("process_ptr",)
 
 class LayerForm(DataModelForm):
     chamber_names = set([x[0] for x in models.six_chamber_chamber_choices])
@@ -89,6 +90,7 @@ class LayerForm(DataModelForm):
         return utils.clean_quantity_field(self.cleaned_data["gas_pre_heat_pressure"], ["mTorr", "mbar"])
     class Meta:
         model = SixChamberLayer
+        exclude = ("deposition",)
 
 class ChannelForm(ModelForm):
     gas_names = set([x[0] for x in models.six_chamber_gas_choices])
@@ -102,6 +104,7 @@ class ChannelForm(ModelForm):
         return self.cleaned_data["gas"]
     class Meta:
         model = SixChamberChannel
+        exclude = ("layer",)
 
 def is_all_valid(deposition_form, layer_forms, channel_forms):
     valid = deposition_form.is_valid()
