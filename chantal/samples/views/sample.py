c7ec118ba7a144aa1af92cf62e5a5cf9810ae756
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-07-30 14:06:01 +0200

    Added My Samples table and "is my sample" checkbox to sample view.

--- chantal/samples/views/sample.py
+++ chantal/samples/views/sample.py
@@ -5,6 +5,7 @@ import string, time, copy
 from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponseRedirect
+import django.forms as forms
 from chantal.samples.models import Sample
 from django.contrib.auth.decorators import login_required
 from . import utils
@@ -58,18 +59,35 @@ def collect_processes(process_context):
     for process in process_context.get_processes():
         processes.append(process_context.digest_process(process))
     return processes
-    
+
+class IsMySampleForm(forms.Form):
+    is_my_sample = forms.BooleanField(label=_(u"is amongst my samples"), required=False)
+
 @login_required
 def show(request, sample_name):
-    start = time.time()
     sample = utils.get_sample(sample_name)
     if not sample:
         raise Http404(_(u"Sample %s could not be found (neither as an alias).") % sample_name)
     if not request.user.has_perm("samples.view_sample") and sample.group not in request.user.groups.all() \
             and sample.currently_responsible_person != request.user:
         return HttpResponseRedirect("permission_error")
+    user_profile = request.user.get_profile()
+    if request.method == "POST":
+        is_my_sample_form = IsMySampleForm(request.POST)
+        if is_my_sample_form.is_valid():
+            if is_my_sample_form.cleaned_data["is_my_sample"]:
+                user_profile.my_samples.add(sample)
+                request.session["success_report"] = _(u"Sample %s was added to Your Samples.") % sample_name
+            else:
+                user_profile.my_samples.remove(sample)
+                request.session["success_report"] = _(u"Sample %s was removed from Your Samples.") % sample_name
+    else:
+        # FixMe: DB access is probably not efficient
+        start = time.time()
+        is_my_sample_form = IsMySampleForm(initial={"is_my_sample": sample in user_profile.my_samples.all()})
+        request.session["db_access_time_in_ms"] = "%.1f" % ((time.time() - start) * 1000)
     processes = collect_processes(ProcessContext(sample))
-    request.session["db_access_time_in_ms"] = "%.1f" % ((time.time() - start) * 1000)
-    return render_to_response("show_sample.html", {"processes": processes, "sample": sample},
+    return render_to_response("show_sample.html", {"processes": processes, "sample": sample,
+                                                   "is_my_sample_form": is_my_sample_form},
                               context_instance=RequestContext(request))
 
