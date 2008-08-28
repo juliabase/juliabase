cd48148b6abbb047dcb43beb0a4c1e725a6c8393
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-08-28 21:11:05 +0200

    Added "edit" link in sample view.  Added proper permission check in edit sample view.

--- chantal/samples/views/sample.py
+++ chantal/samples/views/sample.py
@@ -30,12 +30,14 @@ def edit(request, sample_name):
     sample, redirect = utils.lookup_sample(sample_name, request)
     if redirect:
         return redirect
+    if sample.currently_responsible_person != request.user:
+        return HttpResponseRedirect("permission_error")
     user_details = request.user.get_profile()
     if request.method == "POST":
         sample_form = SampleForm(request.POST, instance=sample)
         if sample_form.is_valid():
             sample_form.save()
-            request.session["success_report"] = _(u"Sample %s was sucessfully changed in the database.") % sample.name
+            request.session["success_report"] = _(u"Sample %s was successfully changed in the database.") % sample.name
             return HttpResponseRedirect("../../samples/%s" % utils.name2url(sample.name))
     else:
         sample_form = SampleForm(instance=sample)
@@ -66,5 +68,6 @@ def show(request, sample_name):
     processes = utils.ProcessContext(request.user, sample).collect_processes()
     request.session["db_access_time_in_ms"] = "%.1f" % ((time.time() - start) * 1000)
     return render_to_response("show_sample.html", {"processes": processes, "sample": sample,
+                                                   "can_edit": request.user == sample.currently_responsible_person,
                                                    "is_my_sample_form": is_my_sample_form},
                               context_instance=RequestContext(request))
