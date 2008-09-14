8d01e6c6b67f32ff3367ee48f07cc62d4510ec34
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-09-14 21:12:08 +0200

    Middleware can now deal with composite language codes like "de-de".

--- chantal/middleware/locale.py
+++ chantal/middleware/locale.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-import locale
+import locale, re
 from django.utils.cache import patch_vary_headers
 from django.utils import translation
 from django.contrib.auth.models import SiteProfileNotAvailable
@@ -18,7 +18,9 @@ class LocaleMiddleware(object):
     translated to the language the user desires (if the language is available,
     of course).
     """
-    def get_language_for_user(self, request):
+    language_pattern = re.compile("[a-zA-Z0-9]+")
+    @staticmethod
+    def get_language_for_user(request):
         if request.user.is_authenticated():
             try:
                 language = request.user.get_profile().language
@@ -26,13 +28,16 @@ class LocaleMiddleware(object):
             except (SiteProfileNotAvailable, UserDetails.DoesNotExist):
                 pass
         return translation.get_language_from_request(request)
+    def get_language_code_only(self, language):
+        match = self.language_pattern.match(language)
+        return match.group(0) if match else "en"
     def process_request(self, request):
         language = self.get_language_for_user(request)
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
         # Now for the locale, but only if necessary because it seems to be a
         # costly operation.
-        new_locale = settings.LOCALES_DICT.get(language)
+        new_locale = settings.LOCALES_DICT.get(self.get_language_code_only(language))
         old_locale = locale.getlocale()[0]
         if not old_locale or not new_locale.startswith(old_locale):
             locale.setlocale(locale.LC_ALL, new_locale or "C")
