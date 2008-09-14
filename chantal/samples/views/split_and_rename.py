b8e8817a5b448b2512947e1e84ad0d479445508a
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-09-14 08:13:10 +0200

    Added docstrings to "split_and_rename.py".

--- chantal/samples/views/split_and_rename.py
+++ chantal/samples/views/split_and_rename.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+u"""Here are the views for an ordinary sample split.
+"""
+
 import datetime
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
@@ -15,6 +18,8 @@ import django.core.urlresolvers
 from chantal.samples.views import utils
 
 class NewNameForm(forms.Form):
+    u"""Form for data of one new sample piece.
+    """
     _ = ugettext_lazy
     new_name = forms.CharField(label=_(u"New sample name"), max_length=30)
     new_purpose = forms.CharField(label=_(u"New sample purpose"), max_length=80, required=False)
@@ -27,6 +32,9 @@ class NewNameForm(forms.Form):
         return self.cleaned_data["new_name"]
 
 class GlobalDataForm(forms.Form):
+    u"""Form for general data for a split as a whole, and for the “finished”
+    checkbox.
+    """
     _ = ugettext_lazy
     finished = forms.BooleanField(label=_(u"All pieces completely entered"), required=False)
     sample_completely_split = forms.BooleanField(label=_(u"Sample was completely split"), initial=True, required=False)
@@ -39,6 +47,29 @@ class GlobalDataForm(forms.Form):
             models.SampleSeries.objects.filter(currently_responsible_person=user_details.user)
 
 def forms_from_post_data(post_data, parent, user_details):
+    u"""Interpret the POST data sent by the user through his browser and create
+    forms from it.  This function also performs the so-called “structural
+    changes”, namely adding and deleting pieces.
+
+    Note this this routine doesn't append the dummy form at the end which can
+    be used by the user to add a new piece.  On the contrary, it ignores it in
+    the POST data if the user didn't make use of it.
+
+    :Parameters:
+      - `post_data`: the value of ``request.POST``
+      - `parent`: the parent sample which is split
+      - `user_details`: the user details of the current user
+
+    :type post_data: ``QueryDict``
+    :type parent: `models.Sample`
+    :type user_details: `models.UserDetails`
+
+    :Return:
+      The list of the pieces forms, the global data form, and whether the
+      structure was changed by the user
+
+    :rtype: list of `NewNameForm`, `GlobalDataForm`, bool
+    """
     new_name_forms = []
     structure_changed = False
     index = 0
@@ -61,16 +92,65 @@ def forms_from_post_data(post_data, parent, user_details):
     return new_name_forms, global_data_form, structure_changed
 
 def forms_from_database(parent, user_details):
+    u"""Generate pristine forms for the given parent.  In particular, this
+    returns an empty list of ``new_name_forms``.
+
+    :Parameters:
+      - `parent`: the sample to be split
+      - `user_details`: the details of the current user
+
+    :type parent: `models.Sample`
+    :type user_details: `sample.UserDetails`
+
+    :Return:
+      the initial ``new_name_forms``, the initial ``global_data_form``
+
+    :rtype: list of `NewNameForm`, list of `GlobalDataForm`
+    """
     new_name_forms = []
     global_data_form = GlobalDataForm(parent, user_details)
     return new_name_forms, global_data_form
 
 def is_all_valid(new_name_forms, global_data_form):
+    u"""Tests the “inner” validity of all forms belonging to this view.  This
+    function calls the ``is_valid()`` method of all forms, even if one of them
+    returns ``False`` (and makes the return value clear prematurely).
+
+    :Parameters:
+      - `new_name_forms`: all “new name forms”, but not the dummy one for new
+        pieces (the one in darker grey).
+      - `global_data_form`: the global data form
+
+    :type new_name_forms: list of `NewNameForm`
+    :type global_data_form: `GlobalDataForm`
+
+    :Return:
+      whether all forms are valid
+
+    :rtype: bool
+    """
     all_valid = all([new_name_form.is_valid() for new_name_form in new_name_forms])
     all_valid = global_data_form.is_valid() and all_valid  # Ordering important: .is_valid() must be called
     return all_valid
 
 def is_referentially_valid(new_name_forms, global_data_form):
+    u"""Test whether all forms are consistent with each other and with the
+    database.  For example, no piece name must occur twice, and the piece names
+    must not exist within the database.
+
+    :Parameters:
+      - `new_name_forms`: all “new name forms”, but not the dummy one for new
+        pieces (the one in darker grey).
+      - `global_data_form`: the global data form
+
+    :type new_name_forms: list of `NewNameForm`
+    :type global_data_form: `GlobalDataForm`
+
+    :Return:
+      whether all forms are consistent with each other and the database
+
+    :rtype: bool
+    """
     referentially_valid = global_data_form.cleaned_data["finished"]
     if not new_name_forms:
         utils.append_error(global_data_form, _(u"You must split into at least one piece."))
@@ -86,6 +166,26 @@ def is_referentially_valid(new_name_forms, global_data_form):
     return referentially_valid
 
 def save_to_database(new_name_forms, global_data_form, parent, sample_split, user):
+    u"""Save all form data to the database.  If `sample_split` is not ``None``,
+    modify it instead of appending a new one.  Warning: For this, the old split
+    process must be the last process at all for the parental sample!  This must
+    be checked before this routine is called.
+
+    :Parameters:
+      - `new_name_forms`: all “new name forms”, but not the dummy one for new
+        pieces (the one in darker grey).
+      - `global_data_form`: the global data form
+      - `parent`: the sample to be split
+      - `sample_split`: the already existing sample split process that is to be
+        modified.  If this is ``None``, create a new one.
+      - `user`: the current user
+
+    :type new_name_forms: list of `NewNameForm`
+    :type global_data_form: `GlobalDataForm`
+    :type parent: `models.Sample`
+    :type sample_split: `models.SampleSplit`
+    :type user: ``django.contrib.auth.models.User``
+    """
     now = datetime.datetime.now()
     if not sample_split:
         sample_split = models.SampleSplit(timestamp=now, operator=user, parent=parent)
@@ -117,6 +217,21 @@ def save_to_database(new_name_forms, global_data_form, parent, sample_split, use
         
 @login_required
 def split_and_rename(request, parent_name=None, old_split_id=None):
+    u"""
+    :Parameters:
+      - `request`: the current HTTP Request object
+      - `parent_name`: if given, the name of the sample to be split
+      - `old_split_id`: if given the process ID of the split to be modified
+
+    :type request: ``HttpRequest``
+    :type parent_name: unicode or ``NoneType``
+    :type old_split_id: int or ``NoneType``
+
+    :Returns:
+      the HTTP response object
+
+    :rtype: ``HttpResponse``
+    """
     assert (parent_name or old_split_id) and not (parent_name and old_split_id)
     if parent_name:
         parent, redirect = utils.lookup_sample(parent_name, request)
