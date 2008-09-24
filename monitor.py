971a2ba21553e92227b7bfd708ce3cedac187853
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-09-24 14:11:15 +0200

    Added Availability analysis.

--- monitor.py
+++ monitor.py
@@ -20,6 +20,8 @@ class SystemInfo(object):
             timestamp, used_mem, used_mem_with_buffers, used_swap, load_avg_5
 
 filename = "/home/bronger/repos/chantal/online/monitor.pickle"
+remote_monitor_log_file_name = "/windows/hobie/remote_monitor.log"
+remote_monitor_pickle_file_name = "/home/bronger/repos/chantal/online/remote_monitor.pickle"
 
 total_pattern = re.compile(r"Mem:\s*(\d+)\s+(\d+)")
 used_pattern = re.compile(r"-/\+ buffers/cache:\s*(\d+)")
@@ -49,6 +51,33 @@ if isinstance(monitor_data[0], tuple):
     for data in old_monitor_data:
         monitor_data.append(SystemInfo(data[0], data[1], data[2], 0, data[3]+1))
 
+class Availability(object):
+    line_pattern = re.compile(ur"(?P<date>[-0-9 :]+)\s+(?P<type>[A-Z]+)\s+(?P<message>.*)")
+    def __init__(self, logfile_name=remote_monitor_log_file_name):
+        self.downtime_intervals = []
+        self.start_of_log = None
+        up = down = 0
+        start = None
+        for line in open(logfile_name):
+            linematch = self.line_pattern.match(line.strip())
+            if linematch:
+                date = datetime.datetime.strptime("%Y-%m-%d %H:%M:%S", linematch.group("date"))
+                if self.start_of_log is None:
+                    self.start_of_log = date
+                type_ = linematch.group("type")
+                if type_ == "ERROR":
+                    if start is None:
+                        start = date
+                    down += 1
+                elif type_ == "INFO":
+                    if start is not None:
+                        self.downtime_intervals.append((start, date))
+                        start = None
+                    up += 1
+        if start is not None:
+            self.downtime_intervals.append((start, date))
+        self.availability = up / (up + down) if up or down else None
+
 while True:
     now = datetime.datetime.now()
     used, used_with_buffers, used_swap = get_free_memory()
@@ -56,4 +85,5 @@ while True:
     while monitor_data and now - monitor_data[0].timestamp > datetime.timedelta(1.1):
         del monitor_data[0]
     pickle.dump(monitor_data, open(filename, "wb"))
+    pickle.dump(Availability(), open(remote_monitor_pickle_file_name, "wb"))
     time.sleep(5*60)
