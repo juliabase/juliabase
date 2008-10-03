d0a72bc9c8c439058ff8ca287b47f7ba9531b9ba
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-10-03 13:24:35 +0200

    Changed name from "parse_session_data" to "default" and updated docstring.

--- chantal/samples/context_processors.py
+++ chantal/samples/context_processors.py
@@ -6,13 +6,17 @@ to `settings.TEMPLATE_CONTEXT_PROCESSORS`.  They add further data to the
 dictionary passed to the templates.
 """
 
-def parse_session_data(request):
+def default(request):
     u"""Injects some session data into the template context.  At the same time,
     it removes them from the session data so that the next view has to re-set
     them.  Obviously, this is meant to things that are displyed only once: the
     last DB access time, a possible success report in green colour à la “sample
-    x was added sucessfully” and the help link on the top (see the
-    `samples.views.utils.help_link` decorator).
+    x was added sucessfully”.
+
+    Additionally, the help link on the top (see the
+    `samples.views.utils.help_link` decorator) is added to the context by
+    extracting it (and removing it from) the request object.  It cannot be in
+    the session because this would reset the cahce every time it is used.
     
     :Parameters:
       - `request`: the current HTTP Request object
