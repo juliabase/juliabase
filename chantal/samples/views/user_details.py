eb680f4d159c895cfed13fc0619b9bcc0f051cf7
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-10-15 18:01:47 +0200

    First complete but un-tested and un-polished version for the user preferences.

--- chantal/samples/views/user_details.py
+++ chantal/samples/views/user_details.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 import django.contrib.auth.models
+from django import forms
+from django.forms.util import ValidationError
+from django.utils.translation import ugettext as _, ugettext_lazy
 from chantal.samples import models
 from chantal.samples.views import utils
 
@@ -31,3 +35,54 @@ def show_user(request, login_name):
     username = models.get_really_full_name(user)
     return render_to_response("show_user.html", {"title": username, "user": user, "userdetails": userdetails},
                               context_instance=RequestContext(request))
+
+class UserDetailsForm(forms.ModelForm):
+    _ = ugettext_lazy
+    def __init__(self, user, *args, **keyw):
+        super(UserDetailsForm, self).__init__(*args, **keyw)
+        self.fields["auto_addition_groups"].queryset = user.groups
+    class Meta:
+        model = models.UserDetails
+        fields = ("auto_addition_groups", "only_important_news")
+
+initials_pattern = re.compile(ur"[A-Z]{2,4}[0-9]*$")
+class InitialsForm(forms.Form):
+    _ = ugettext_lazy
+    initials = forms.CharField(label=_(u"Initials"), min_length=2, max_length=4, required=False)
+    def __init__(self, user, *args, **keyw):
+        super(InitialsForm, self).__init__(*args, **keyw)
+        if user.initials:
+            self.fields["initials"].widget.attrs["readonly"] = "readonly"
+    def clean_initials(self):
+        initials = self.cleaned_data["initials"]
+        if not initials:
+            return initials
+        # Note that minimal and maximal length are already checked.
+        if not initials_pattern.match(initials):
+            raise ValidationError(_(u"The initials must start with two uppercase letters.  "
+                                    u"They must contain uppercase letters and digits only.  Digits must be at the end."))
+        if models.Initials.objects.filter(initials=initials).count():
+            raise ValidationError(_(u"These initials are already used."))
+
+@login_required
+def edit_preferences(request, login_name):
+    user = get_object_or_404(django.contrib.auth.models.User, username=login_name)
+    if not request.user.is_staff and request.user != user:
+        return utils.HttpResponseSeeOther("permission_error")
+    user_details = utils.get_profile(user)
+    if request.method == "POST":
+        user_details_form = UserDetailsForm(user, request.POST, instance=user_details)
+        initials_form = InitialsForm(user, request.POST)
+        if user_details_form.is_valid() and initials_form.is_valid():
+            user_details_form.save()
+            initials = initials_form.cleaned_data["initials"]
+            if initials and models.Initials.objects.filter(user=user).count() == 0:
+                models.Initials.objects.create(initials=initials, user=user)
+            return utils.successful_response(request, _(u"The preferences were successfully updated."))
+    else:
+        user_details_form = UserDetailsForm(user, instance=user_details)
+        initials_form = InitialsForm(user)
+    return render_to_response("edit_preferences.html", {"title": login_name, "user_details": user_details_form,
+                                                        "initials": initials_form},
+                              context_instance=RequestContext(request))
+    
