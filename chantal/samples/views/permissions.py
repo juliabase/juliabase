3e82e9841b550fe9efa6dbd3e55b5cddb4049b64
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-10-19 12:54:16 +0200

    Added first (untested) assertion function.  Added some docstrings.

--- chantal/samples/views/permissions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from django.utils.translation import ugettext as _
-
-class PermissionError(Exception):
-    def __init__(self, description):
-        super(PermissionError, self).__init__(_(u"Permission missing: ") + description)
-        self.description = description
