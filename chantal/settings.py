d121d147c629695e1145de18f228655c5f0bb15e
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-02-19 03:27:39 +0100

    Switched to PostgreSQL.

--- chantal/settings.py
+++ chantal/settings.py
@@ -25,10 +25,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = 'chantal'      # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'         # Not used with sqlite3.
-DATABASE_PASSWORD = CREDENTIALS["mysql_password"]    # Not used with sqlite3.
+DATABASE_USER = 'postgres'     # Not used with sqlite3.
+DATABASE_PASSWORD = CREDENTIALS["postgresql_password"]    # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
