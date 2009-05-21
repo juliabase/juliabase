a8dd96aa86158e14ac9980bf05b057d13066f2df
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-05-21 13:00:07 +0200

    Added all necessary hooks for users and groups.  Untested.

--- django_refdb/refdb/__init__.py
+++ django_refdb/refdb/__init__.py
@@ -4,17 +4,38 @@
 from __future__ import absolute_import
 
 import django.contrib.auth.models
-from django.dispatch import dispatcher
 from django.db.models import signals
 import pyrefdb
 from django.conf import settings
 from . import utils
 
 
-def user_created(sender, **kwargs):
+def add_refdb_user(sender, **kwargs):
     user = kwargs["instance"]
-    password = utils.get_refdb_password(user)
-    pyrefdb.Connection(settings.CREDENTIALS["postgresql_user"], settings.CREDENTIALS["postgresql_password"]).\
-        add_user("drefdbuser%d" % kwargs["instance"].id, password)
+    utils.get_refdb_connection("root").add_user(utils.refdb_username(user.id), utils.get_refdb_password(user))
 
-signals.post_save.connect(user_created, django.contrib.auth.models.User)
+signals.pre_save.connect(add_refdb_user, sender=django.contrib.auth.models.User)
+
+
+def remove_refdb_user(sender, **kwargs):
+    user = kwargs["instance"]
+    utils.get_refdb_connection("root").remove_user(utils.refdb_username(user.id))
+
+signals.pre_delete.connect(remove_refdb_user, sender=django.contrib.auth.models.User)
+
+
+def add_refdb_group(sender, **kwargs):
+    group = kwargs["instance"]
+    note = pyrefdb.XNote()
+    note.citation_key = "django-refdb-group-%d" % group.id
+    utils.get_refdb_connection("root").add_extended_note(note)
+
+signals.pre_save.connect(add_refdb_group, sender=django.contrib.auth.models.Group)
+
+
+def remove_refdb_group(sender, **kwargs):
+    group = kwargs["instance"]
+    id_ = utils.get_refdb_connection("root").get_extended_notes(":NCK:=django-refdb-group-%d" % group.id)[0].id
+    utils.get_refdb_connection("root").delete_extended_note([id_])
+
+signals.pre_delete.connect(remove_refdb_group, sender=django.contrib.auth.models.Group)
