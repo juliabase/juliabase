0d1b9b9c34800763b03da2c7ee334cee1b03ae2d
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-05-24 16:49:45 +0200

    Added docstring.

--- django_refdb/refdb/models.py
+++ django_refdb/refdb/models.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+u"""Empty module.  All data is stored in RefDB, so Django-RefDB doesn't need
+models of its own.  Only the models from the “auth” app are used (``User``,
+``Group``).
+"""
