4ec9ee7d803ec84a27dcf0cd7ecaee985aa460e2
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-07-23 22:17:24 +0200

    Added main reference list and simple search form to the main menu.

--- django_refdb/refdb/views/main.py
+++ django_refdb/refdb/views/main.py
@@ -5,8 +5,37 @@ from __future__ import absolute_import
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django import forms
+from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _, ungettext, ugettext_lazy
+from .. import utils
 
 
+class SimpleSearchForm(forms.Form):
+    u"""Form class for the simple search filters.  Currently, it only accepts a
+    RefDB query string.
+    """
+
+    _ = ugettext_lazy
+    query_string = forms.CharField(label=_("Query string"), required=False)
+
+
+@login_required
 def main_menu(request):
-    return render_to_response("main_menu.html", {"title": _(u"Main menu")}, context_instance=RequestContext(request))
+    u"""Generates the main page with simple search and main reference list.
+
+    :Parameters:
+      - `request`: the current HTTP Request object
+
+    :type request: ``HttpRequest``
+
+    :Returns:
+      the HTTP response object
+
+    :rtype: ``HttpResponse``
+    """
+    search_form = SimpleSearchForm()
+    references = utils.get_refdb_connection(request.user).get_references(":ID:>0", listname=utils.refdb_username(request.user.pk))
+    print len(references)
+    return render_to_response("main_menu.html", {"title": _(u"Main menu"), "search": search_form, "references": references},
+                              context_instance=RequestContext(request))
