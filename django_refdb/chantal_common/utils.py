fd5190259d2ec17160f985443a616728ac3a3e5a
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-11-04 16:34:59 +0100

    Moved template filters to chantal_common.

--- django_refdb/chantal_common/utils.py
+++ django_refdb/chantal_common/utils.py
@@ -10,6 +10,9 @@
 
 from __future__ import absolute_import
 
+import codecs, re, os.path
+import django.http
+
 
 class HttpResponseUnauthorized(django.http.HttpResponse):
     u"""The response sent back in case of a permission error.  This is another
@@ -17,3 +20,57 @@ class HttpResponseUnauthorized(django.http.HttpResponse):
     trivial code.
     """
     status_code = 401
+
+
+entities = {}
+for line in codecs.open(os.path.join(os.path.dirname(__file__), "entities.txt"), encoding="utf-8"):
+    entities[line[:12].rstrip()] = line[12]
+entity_pattern = re.compile(r"&[A-Za-z0-9]{2,8};")
+
+def substitute_html_entities(text):
+    u"""Searches for all ``&entity;`` named entities in the input and replaces
+    them by their unicode counterparts.  For example, ``&alpha;``
+    becomes ``α``.  Escaping is not possible unless you spoil the pattern with
+    a character that is later removed.  But this routine doesn't have an
+    escaping mechanism.
+
+    :Parameters:
+      - `text`: the user's input to be processed
+
+    :type text: unicode
+
+    :Return:
+      ``text`` with all named entities replaced by single unicode characters
+
+    :rtype: unicode
+    """
+    result = u""
+    position = 0
+    while position < len(text):
+        match = entity_pattern.search(text, position)
+        if match:
+            start, end = match.span()
+            character = entities.get(text[start+1:end-1])
+            result += text[position:start] + character if character else text[position:end]
+            position = end
+        else:
+            result += text[position:]
+            break
+    return result
+
+
+def get_really_full_name(user):
+    u"""Unfortunately, Django's ``get_full_name`` method for users returns the
+    empty string if the user has no first and surname set.  However, it'd be
+    sensible to use the login name as a fallback then.  This is realised here.
+
+    :Parameters:
+      - `user`: the user instance
+    :type user: ``django.contrib.auth.models.User``
+
+    :Return:
+      The full, human-friendly name of the user
+
+    :rtype: unicode
+    """
+    return user.get_full_name() or unicode(user)
--- django_refdb/refdb/templatetags/refdb_extras.py
+++ django_refdb/refdb/templatetags/refdb_extras.py
@@ -25,62 +25,6 @@ from refdb.views import utils
 register = template.Library()
 
 
-# FixMe: This is a duplicate of Chantal
-
-entities = {}
-for line in codecs.open(os.path.join(os.path.dirname(__file__), "entities.txt"), encoding="utf-8"):
-    entities[line[:12].rstrip()] = line[12]
-
-entity_pattern = re.compile(r"&[A-Za-z0-9]{2,8};")
-
-def substitute_html_entities(text):
-    u"""Searches for all ``&entity;`` named entities in the input and replaces
-    them by their unicode counterparts.  For example, ``&alpha;``
-    becomes ``α``.  Escaping is not possible unless you spoil the pattern with
-    a character that is later removed.  But this routine doesn't have an
-    escaping mechanism.
-
-    :Parameters:
-      - `text`: the user's input to be processed
-
-    :type text: unicode
-
-    :Return:
-      ``text`` with all named entities replaced by single unicode characters
-
-    :rtype: unicode
-    """
-    result = u""
-    position = 0
-    while position < len(text):
-        match = entity_pattern.search(text, position)
-        if match:
-            start, end = match.span()
-            character = entities.get(text[start+1:end-1])
-            result += text[position:start] + character if character else text[position:end]
-            position = end
-        else:
-            result += text[position:]
-            break
-    return result
-
-
-@register.filter
-@stringfilter
-def markdown(value):
-    u"""Filter for formatting the value by assuming Markdown syntax.  Embedded
-    HTML tags are always escaped.  Warning: You need at least Python Markdown
-    1.7 or later so that this works.
-
-    FixMe: Before Markdown sees the text, all named entities are replaced, see
-    `samples.views.utils.substitute_html_entities`.  This creates a mild
-    escaping problem.  ``\&amp;`` becomes ``&amp;amp;`` instead of ``\&amp;``.
-    It can only be solved by getting python-markdown to replace the entities,
-    however, I can't easily do that without allowing HTML tags, too.
-    """
-    return markup.markdown(escape(substitute_html_entities(unicode(value))))
-
-
 @register.filter
 def concise_title(value):
     u"""Filter to pick the abbreviated title of a publication or series with
@@ -104,65 +48,6 @@ def journal(value):
     if value.type in ["ABST", "INPR", "JOUR", "JFULL", "MGZN", "NEWS"]:
         return utils.prettyprint_title_abbreviation(value.publication.title_abbrev) if value.publication.title_abbrev \
             else value.publication.title
-
-
-# FixMe: This is a duplicate of Chantal
-
-@register.filter
-@stringfilter
-def urlquote_plus(value):
-    u"""Filter for quoting URLs so that they can be used within other URLs.
-    This is useful for added “next” URLs in query strings, for example::
-
-        <a href="{{ process.edit_url }}?next={{ sample.get_absolute_url|urlquote_plus }}"
-               >{% trans 'edit' %}</a>
-    """
-    return django.utils.http.urlquote_plus(value, safe="/")
-urlquote_plus.is_safe = False
-
-
-# FixMe: This is a duplicate of Chantal
-
-@register.simple_tag
-def input_field(field):
-    u"""Tag for inserting a field value into an HTML table as an editable
-    field.  It consists of two ``<td>`` elements, one for the label and one for
-    the value, so it spans two columns.  This tag is primarily used in
-    tamplates of edit views.  Example::
-
-        {% input_field deposition.number %}
-    """
-    result = u"""<td class="label"><label for="id_%(html_name)s">%(label)s:</label></td>""" % \
-        {"html_name": field.html_name, "label": field.label}
-    help_text = u""" <span class="help">(%s)</span>""" % field.help_text if field.help_text else u""
-    result += u"""<td class="input">%(field)s%(help_text)s</td>""" % {"field": field, "help_text": help_text}
-    return result
-
-
-# FixMe: This is a duplicate of Chantal
-
-@register.inclusion_tag("error_list.html")
-def error_list(form, form_error_title, outest_tag=u"<table>"):
-    u"""Includes a comprehensive error list for one particular form into the
-    page.  It is an HTML table, so take care that the tags are nested
-    properly.  Its template can be found in the file ``"error_list.html"``.
-
-    :Parameters:
-      - `form`: the bound form whose errors should be displayed; if ``None``,
-        nothing is generated
-      - `form_error_title`: The title used for general error messages.  These
-        are not connected to one particular field but the form as a
-        whole. Typically, they are generated in the ``is_referentially_valid``
-        functions.
-      - `outest_tag`: May be ``"<table>"`` or ``"<tr>"``, with ``"<table>"`` as
-        the default.  It is the outmost HTML tag which is generated for the
-        error list.
-
-    :type form: ``forms.Form``
-    :type form_error_title: unicode
-    :type outest_tag: unicode
-    """
-    return {"form": form, "form_error_title": form_error_title, "outest_tag": outest_tag}
 
 
 class FlexibleFieldNode(template.Node):
