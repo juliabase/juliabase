415aa006abc8e7cb33eedaafe8c5410965b3458a
Torsten Bronger <bronger@physik.rwth-aachen.de>
2009-11-04 19:06:00 +0100

    Added first view, namely the markdown sandbox.

--- django_refdb/chantal_common/utils.py
+++ django_refdb/chantal_common/utils.py
@@ -3,15 +3,16 @@
 #
 # Copyright (c) 2009 Torsten Bronger <bronger@physik.rwth-aachen.de>
 #
-# This file is part of Django-RefDB.  Django-RefDB is published under the MIT
-# license.  A copy of this licence is shipped with Django-RefDB in the file
-# LICENSE.
+# This file is part of Chantal.  Chantal is published under the MIT license.  A
+# copy of this licence is shipped with Chantal in the file LICENSE.
 
 
 from __future__ import absolute_import
 
 import codecs, re, os.path
+from functools import update_wrapper
 import django.http
+from django.forms.util import ErrorList, ValidationError
 
 
 class HttpResponseUnauthorized(django.http.HttpResponse):
@@ -74,3 +75,78 @@ def get_really_full_name(user):
     :rtype: unicode
     """
     return user.get_full_name() or unicode(user)
+
+
+def append_error(form, error_message, fieldname="__all__"):
+    u"""This function is called if a validation error is found in form data
+    which cannot be found by the ``is_valid`` method itself.  The reason is
+    very simple: For many types of invalid data, you must take other forms in
+    the same view into account.
+
+    See, for example, `split_after_deposition.is_referentially_valid`.
+
+    :Parameters:
+      - `form`: the form to which the erroneous field belongs
+      - `error_message`: the message to be presented to the user
+      - `fieldname`: the name of the field that triggered the validation
+        error.  It is optional, and if not given, the error is considered an
+        error of the form as a whole.
+
+    :type form: ``forms.Form`` or ``forms.ModelForm``.
+    :type fieldname: str
+    :type error_message: unicode
+    """
+    # FixMe: Is it really a good idea to call ``is_valid`` here?
+    # ``append_error`` is also called in ``clean`` methods after all.
+    form.is_valid()
+    form._errors.setdefault(fieldname, ErrorList()).append(error_message)
+
+
+dangerous_markup_pattern = re.compile(r"([^\\]|\A)!\[|[\n\r][-=]")
+def check_markdown(text):
+    u"""Checks whether the Markdown input by the user contains only permitted
+    syntax elements.  I forbid images and headings so far.
+
+    :Parameters:
+      - `text`: the Markdown input to be checked
+
+    :Exceptions:
+      - `ValidationError`: if the ``text`` contained forbidden syntax
+        elements.
+    """
+    if dangerous_markup_pattern.search(text):
+        raise ValidationError(_(u"You mustn't use image and headings syntax in Markdown markup."))
+
+
+class _AddHelpLink(object):
+    u"""Internal helper class in order to realise the `help_link` function
+    decorator.
+    """
+
+    def __init__(self, original_view_function, help_link):
+        self.original_view_function = original_view_function
+        self.help_link = help_link
+        update_wrapper(self, original_view_function)
+
+    def __call__(self, request, *args, **kwargs):
+        request.chantal_help_link = self.help_link
+        return self.original_view_function(request, *args, **kwargs)
+
+
+def help_link(link):
+    u"""Function decorator for views functions to set a help link for the view.
+    The help link is embedded into the top line in the layout, see the template
+    ``base.html``.  Currently, it is prepended with ``"/trac/chantal/wiki/"``.
+
+    :Parameters:
+      - `link`: the relative URL to the help page.
+
+    :type link: str
+    """
+
+    def decorate(original_view_function):
+        return _AddHelpLink(original_view_function, link)
+
+    return decorate
+
+
---
+++ django_refdb/chantal_common/views.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2009 Torsten Bronger <bronger@physik.rwth-aachen.de>
+#
+# This file is part of Chantal.  Chantal is published under the MIT license.  A
+# copy of this licence is shipped with Chantal in the file LICENSE.
+
+
+from __future__ import absolute_import
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+import django.forms as forms
+from django.utils.translation import ugettext as _, ugettext_lazy
+from . import utils
+from .utils import help_link
+
+
+class SandboxForm(forms.Form):
+    u"""Form for entering Markdown markup just for testing it.
+    """
+    _ = ugettext_lazy
+    sandbox = forms.CharField(label=_(u"Sandbox"), widget=forms.Textarea, required=False)
+
+    def clean_sandbox(self):
+        u"""Forbid image and headings syntax in Markdown markup.
+        """
+        sandbox = self.cleaned_data["sandbox"]
+        utils.check_markdown(sandbox)
+        return sandbox
+
+
+@help_link(_(u"MarkdownMarkup"))
+def sandbox(request):
+    u"""View so that the user can test Markdown syntax.  I deliberately decided
+    not to *explain* Markdown on this page.  Rather, I recommend the help page
+    in the Wiki.
+
+    :Parameters:
+      - `request`: the current HTTP Request object
+
+    :type request: ``HttpRequest``
+
+    :Returns:
+      the HTTP response object
+
+    :rtype: ``HttpResponse``
+    """
+    extracted_content = None
+    if request.method == "POST":
+        sandbox_form = SandboxForm(request.POST)
+        if sandbox_form.is_valid():
+            extracted_content = sandbox_form.cleaned_data["sandbox"]
+    else:
+        sandbox_form = SandboxForm()
+    return render_to_response("markdown_sandbox.html", {"title": _(u"Markdown sandbox"), "sandbox": sandbox_form,
+                                                        "extracted_content": extracted_content},
+                              context_instance=RequestContext(request))
