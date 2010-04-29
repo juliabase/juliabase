d25a5138c50e5974803fc9071ac26e0bcdf5d8f3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-04-29 16:58:32 +0200

    Fixed last occurences of "project".

--- chantal_common/admin.py
+++ chantal_common/admin.py
@@ -4,7 +4,7 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
-from chantal_common.models import UserDetails, Project
+from chantal_common.models import UserDetails, Topic
 
 admin.site.register(UserDetails)
-admin.site.register(Project)
+admin.site.register(Topic)
