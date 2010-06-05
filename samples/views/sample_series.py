f72517dec64d80032052564d81048b2c6734242f
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-06-05 20:50:54 +0200

    Added missing import.

--- samples/views/sample_series.py
+++ samples/views/sample_series.py
@@ -12,6 +12,7 @@ be changed once they have been created.
 from __future__ import absolute_import
 
 import datetime
+from django.views.decorators.http import last_modified
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
