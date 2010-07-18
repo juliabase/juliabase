32bb78f29a56d1e7988104de1ff833f8c3fe599b
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-07-18 15:43:35 +0200

    Removed year from new style names with external operators in bulk rename.

--- samples/views/bulk_rename.py
+++ samples/views/bulk_rename.py
@@ -23,17 +23,19 @@ from samples import models, permissions
 from samples.views import utils
 
 
-class InitialsForm(forms.Form):
-    u"""Form for giving the initials to be used for the new names.  This form
-    is not used if the user has only his own initials available, i.e. there is
-    no external operator with own initials connected with this user.
+class PrefixesForm(forms.Form):
+    u"""Form for giving the prefix to be used for the new names.  This form is
+    not used if the user has only his own initials available, i.e. there is no
+    external operator with own initials connected with this user.
+
+    Prefixes may be ``10-TB-`` or ``ACME-`` (for external operators).
     """
     _ = ugettext_lazy
-    initials = forms.ChoiceField(label=_(u"Initials"))
+    prefix = forms.ChoiceField(label=_(u"Prefix"))
 
-    def __init__(self, available_initials, *args, **kwargs):
-        super(InitialsForm, self).__init__(*args, **kwargs)
-        self.fields["initials"].choices = available_initials
+    def __init__(self, available_prefixes, *args, **kwargs):
+        super(PrefixesForm, self).__init__(*args, **kwargs)
+        self.fields["prefix"].choices = available_prefixes
 
 
 class NewNameForm(forms.Form):
@@ -42,21 +44,18 @@ class NewNameForm(forms.Form):
     _ = ugettext_lazy
     name = forms.CharField(label=_(u"New name"), max_length=22)
 
-    def __init__(self, year, initials, *args, **kwargs):
+    def __init__(self, prefix_, *args, **kwargs):
         u"""Class constructor.
 
         :Parameters:
-          - `year`: the two-digit year of origin
-          - `initials`: The initials to be used.  If, for some reason, there
-            are no initials available, give an empty string.  Validation will
-            then fail, however, it would fail for the whole page anyway without
-            initials.
+          - `prefix_`: The prefix to be used.  If, for some reason, there is no
+            prefix available, give an empty string.  Validation will then fail,
+            however, it would fail for the whole page anyway without a prefix.
 
-        :type year: str
-        :type initials: str
+        :type prefix_: str
         """
         super(NewNameForm, self).__init__(*args, **kwargs)
-        self.prefix_ = "%s-%s-" % (year, initials)
+        self.prefix_ = prefix_
 
     def clean_name(self):
         new_name = self.prefix_ + self.cleaned_data["name"]
@@ -111,11 +110,13 @@ def bulk_rename(request):
 
     :rtype: ``HttpResponse``
     """
+    year = datetime.date.today().strftime("%y")
     try:
         own_initials = request.user.initials
-        available_initials = [(own_initials.pk, unicode(own_initials))]
+        own_prefix = u"{0}-{1}-".format(year, own_initials)
+        available_prefixes = [(own_prefix, own_prefix)]
     except models.Initials.DoesNotExist:
-        available_initials = []
+        available_prefixes = []
     numbers_list = utils.parse_query_string(request).get("numbers", "")
     samples = [get_object_or_404(models.Sample, name="*"+number.zfill(5)) for number in numbers_list.split(",")]
     if not samples:
@@ -123,26 +124,26 @@ def bulk_rename(request):
                         u"zeros) as a comma-separated list without whitespace in the “numbers” query string parameter."))
     for sample in samples:
         permissions.assert_can_edit_sample(request.user, sample)
-    year = u"%02d" % (datetime.date.today().year % 100)
     for external_operator in request.user.external_contacts.all():
         try:
             operator_initials = external_operator.initials
         except models.Initials.DoesNotExist:
             continue
-        available_initials.append((operator_initials.pk, unicode(operator_initials)))
-    if not available_initials:
+        prefix = u"{0}-".format(operator_initials)
+        available_prefixes.append((prefix, prefix))
+    if not available_prefixes:
         query_string = "initials_mandatory=True&next=" + django.utils.http.urlquote_plus(
             request.path + "?" + request.META["QUERY_STRING"], safe="/")
         messages.info(request, _(u"You may change the sample names, but you must choose initials first."))
         return utils.successful_response(request, view="samples.views.user_details.edit_preferences",
                                          kwargs={"login_name": request.user.username},
                                          query_string=query_string, forced=True)
-    single_initials = available_initials[0][1] if len(available_initials) == 1 else None
+    single_prefix = available_prefixes[0][1] if len(available_prefixes) == 1 else None
     if request.method == "POST":
-        initials_form = InitialsForm(available_initials, request.POST)
-        initials = single_initials or (initials_form.cleaned_data["initials"] if initials_form.is_valid() else u"")
-        new_name_forms = [NewNameForm(year, initials, request.POST, prefix=str(sample.pk)) for sample in samples]
-        all_valid = initials_form.is_valid() or bool(single_initials)
+        prefixes_form = PrefixesForm(available_prefixes, request.POST)
+        prefix = single_prefix or (prefixes_form.cleaned_data["prefix"] if prefixes_form.is_valid() else u"")
+        new_name_forms = [NewNameForm(prefix, request.POST, prefix=str(sample.pk)) for sample in samples]
+        all_valid = prefixes_form.is_valid() or bool(single_prefix)
         all_valid = all([new_name_form.is_valid() for new_name_form in new_name_forms]) and all_valid
         referentially_valid = is_referentially_valid(new_name_forms)
         if all_valid and referentially_valid:
@@ -151,10 +152,10 @@ def bulk_rename(request):
                 sample.save()
             return utils.successful_response(request, _(u"Successfully renamed the samples."))
     else:
-        initials_form = InitialsForm(available_initials, initial={"initials": available_initials[0][0]})
-        new_name_forms = [NewNameForm(year, u"", prefix=str(sample.pk)) for sample in samples]
+        prefixes_form = PrefixesForm(available_prefixes, initial={"prefix": available_prefixes[0][0]})
+        new_name_forms = [NewNameForm(u"", prefix=str(sample.pk)) for sample in samples]
     return render_to_response("samples/bulk_rename.html",
                               {"title": _(u"Giving new-style names"),
-                               "initials": initials_form, "single_initials": single_initials,
+                               "prefixes": prefixes_form, "single_prefix": single_prefix,
                                "samples": zip(samples, new_name_forms), "year": year},
                               context_instance=RequestContext(request))
