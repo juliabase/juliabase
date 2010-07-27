3b113dbb3d17b153d7cd5f0cbdac8fc6988852fb
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-07-27 19:06:18 +0200

    Improved docstring.

--- samples/views/form_utils.py
+++ samples/views/form_utils.py
@@ -446,21 +446,23 @@ class OperatorField(forms.ChoiceField):
     operator, and hide the field from display by ``style="display: none"`` in
     the HTML template.
 
-    If you want to use this field, do the following things:
+    If you want to use this field, do the following things::
 
-    1. This field must be made required
+        1. This field must be made required
 
-    2. If the user is not staff, make the possible choices of the external
-       operator field empty.
+        2. If the user is not staff, make the possible choices of the external
+           operator field empty.
 
-    3. Assure in your ``clean()`` method that non-staff doesn't submit an
-       external operator.  In the same method, say if the ``external_operator``
-       field was empty::
+        3. Assure in your ``clean()`` method that non-staff doesn't submit an
+           external operator.  In the same method, say if the
+           ``external_operator`` field was empty::
 
-           self.cleaned_data["external_operator"] = \
-               self.fields["operator"].external_operator
+               self.cleaned_data["external_operator"] = \
+                   self.fields["operator"].external_operator
 
-    4. In the template, show the external operator field only for staff.
+        4. In the template, show the external operator field only for staff.
+
+    A good example is in ``substrate.py`` of the IPV adaption of Chantal.
     """
 
     def set_choices(self, user, old_process):
