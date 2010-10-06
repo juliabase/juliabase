2ce4a71099a51d7a052ac0e96ba9fa2abc00deb3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-10-06 13:14:59 +0200

    Added doctsrings.  Suppressed tags for users not allowed to view them.

--- samples/templatetags/samples_extras.py
+++ samples/templatetags/samples_extras.py
@@ -316,6 +316,14 @@ def flatten_multiline_text(value, separator=u" ● "):
     return separator.join(lines)
 
 
+@register.filter
+def with_tags(sample, user):
+    u"""Shows the sample name with tags.  The tags are shortened.  Moreover,
+    they are suppressed if the user is not allowed to view them.
+    """
+    return sample.name_with_tags(user)
+
+
 class ValueFieldNode(template.Node):
     u"""Helper class to realise the `value_field` tag.
     """
