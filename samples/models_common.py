03f067d5556b462dbeddf1827dc2fa8df92fdd0b
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-10-30 18:44:34 +0200

    Added related models to searches for Processes.

--- samples/models_common.py
+++ samples/models_common.py
@@ -553,16 +553,21 @@ class Process(PolymorphicModel):
     def get_model_field(cls):
         attributes = []
         for field in cls._meta.fields:
-            if field.name not in ["timestamp_inaccuracy", "cache_keys", "last_modified"]:
+            if field.name not in ["id", "actual_object_id", "timestamp_inaccuracy", "cache_keys", "last_modified"]:
                 if field.choices:
                     attributes.append(OptionChoiceField(cls, field.name))
                 elif type(field) in [models.CharField, models.TextField]:
                     attributes.append(OptionTextField(cls, field.name))
-                elif type(field) in [models.IntegerField, models.FloatField, models.DecimalField]:
+                elif type(field) in [models.AutoField, models.BigIntegerField, models.IntegerField, models.FloatField,
+                                     models.DecimalField, models.PositiveIntegerField, models.PositiveSmallIntegerField,
+                                     models.SmallIntegerField]:
                     attributes.append(OptionIntervalField(cls, field.name))
                 elif type(field) == models.DateTimeField:
                     attributes.append(OptionDateTimeField(cls, field.name))
         related_models = {Sample: "samples"}
+        related_models.update(
+            (related_object.model, related_object.field.name) for related_object in cls._meta.get_all_related_objects()
+            if not related_object.model.__name__.startswith("Feed"))
         return ModelField(cls, related_models, attributes)
 
 
