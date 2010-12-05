35553ba605360278279ab7c13c36fa15eea573fc
Torsten Bronger <bronger@physik.rwth-aachen.de>
2010-12-05 23:55:53 +0100

    Fixed advanced search for abstract processes in samples.

--- samples/models_common.py
+++ samples/models_common.py
@@ -916,6 +916,7 @@ class Sample(models.Model):
                          search.TextSearchField(cls, "tags"), search.TextNullSearchField(cls, "topic", "name")]
         related_models = dict((model, "processes") for model in get_all_searchable_physical_processes())
         related_models[Result] = "processes"
+        related_models[Process] = "processes"
         return search.SearchTreeNode(cls, related_models, search_fields)
 
 
