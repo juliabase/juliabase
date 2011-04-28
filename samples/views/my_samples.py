3d052ec35d095976e994b9ae0a51d7d3f6817a1c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2011-04-28 14:14:40 +0200

    Fixed clearances for the edit view.

--- samples/views/my_samples.py
+++ samples/views/my_samples.py
@@ -67,8 +67,8 @@ class ActionForm(forms.Form):
         self.fields["new_currently_responsible_person"].set_users(user)
         self.fields["copy_to_user"].set_users_without(user)
         self.fields["new_topic"].set_topics(user)
-        clearance_sets = sorted((utils.capitalize_first_letter(ugettext(name)), models.clearance_sets[name])
-                                for name in models.clearance_sets, 2)
+        clearance_sets = sorted((utils.capitalize_first_letter(ugettext(name)), models)
+                                for name, models in models.clearance_sets.iteritems())
         clearance_sets = [(str(i),) + clearance_set for i, clearance_set in enumerate(clearance_sets, 2)]
         self.fields["clearance"].choices = [("", u"---------"), ("0", _(u"sample only")),
                                             ("1", _(u"all processes up to now"))]
