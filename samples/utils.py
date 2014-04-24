d20c84006011ae64a697e1f001abe37d6dc6eaaf
Marvin Goblet <m.goblet@fz-juelich.de>
2014-04-24 17:03:10 +0200

    Added function to automatically add all models to one department.

--- samples/utils.py
+++ samples/utils.py
@@ -15,7 +15,10 @@
 from __future__ import absolute_import, division, unicode_literals
 from chantal_common.models import Department
 from django.contrib.contenttypes.models import ContentType
-
+from chantal_common import utils
+from django.conf import settings
+from django.db.utils import DatabaseError
+import sys
 
 def register_to_department(process_cls, department_name):
     """Connects a process with a department.
@@ -27,7 +30,24 @@ def register_to_department(process_cls, department_name):
     :type process_cls: ``samples.models.Process``
     :type department_name: str
     """
-    department = Department.objects.get(name=department_name)
-    content_type = ContentType.objects.get_for_model(process_cls)
-    if content_type not in department.processes.iterator():
-        department.processes.add(content_type)
+    if not "syncdb" in sys.argv:
+        try:
+            department = Department.objects.get(name=department_name)
+            content_type = ContentType.objects.get_for_model(process_cls)
+            if content_type not in department.processes.iterator():
+                department.processes.add(content_type)
+        except (DatabaseError, Department.DoesNotExist):
+            pass
+
+
+def register_all_models_to_department(department_name):
+    """Connects all database models from an chantal app to the belonging department.
+
+    :Parameter:
+      - `department`: the name of the department to which the processes belongs
+
+    :type department_name: str
+    """
+    for process_class in utils.get_all_models(settings.MAP_DEPARTMENTS_TO_APP_LABELS[department_name]).itervalues():
+        if not process_class._meta.abstract == True:
+            register_to_department(process_class, department_name)
