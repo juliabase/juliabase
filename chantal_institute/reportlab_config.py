2e243ed86dcbd842943b8ddadd1eb5396eb26365
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-08-04 09:38:27 +0200

    Made robust against unavailable DejaVu font.

--- chantal_institute/reportlab_config.py
+++ chantal_institute/reportlab_config.py
@@ -20,12 +20,13 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 
-dejavu_root = "/usr/share/fonts/truetype/ttf-dejavu"
-default_fontname = "DejaVu"
-pdfmetrics.registerFont(TTFont("DejaVu", os.path.join(dejavu_root, "DejaVuSans.ttf")))
-pdfmetrics.registerFont(TTFont("DejaVuOb", os.path.join(dejavu_root, "DejaVuSans-Oblique.ttf")))
-pdfmetrics.registerFont(TTFont("DejaVuBd", os.path.join(dejavu_root, "DejaVuSans-Bold.ttf")))
-pdfmetrics.registerFont(TTFont("DejaVuBdOb", os.path.join(dejavu_root, "DejaVuSans-BoldOblique.ttf")))
-pdfmetrics.registerFontFamily(default_fontname, normal="DejaVu", bold="DejaVuBd", italic="DejaVuOb",
-                              boldItalic="DejaVuBdOb")
-reportlab.rl_config.canvas_basefontname = default_fontname
+dejavu_root = "/usr/share/fonts/truetype/dejavu"
+if os.path.exists(os.path.join(dejavu_root, "DejaVuSans.ttf")):
+    pdfmetrics.registerFont(TTFont("DejaVu", os.path.join(dejavu_root, "DejaVuSans.ttf")))
+    pdfmetrics.registerFont(TTFont("DejaVuOb", os.path.join(dejavu_root, "DejaVuSans-Oblique.ttf")))
+    pdfmetrics.registerFont(TTFont("DejaVuBd", os.path.join(dejavu_root, "DejaVuSans-Bold.ttf")))
+    pdfmetrics.registerFont(TTFont("DejaVuBdOb", os.path.join(dejavu_root, "DejaVuSans-BoldOblique.ttf")))
+    default_fontname = "DejaVu"
+    pdfmetrics.registerFontFamily(default_fontname, normal="DejaVu", bold="DejaVuBd", italic="DejaVuOb",
+                                  boldItalic="DejaVuBdOb")
+    reportlab.rl_config.canvas_basefontname = default_fontname
