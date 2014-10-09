24d3861a74b22798d3ebed8dbcb3013df26b12b3
marvin <m.goblet@fz-juelich.de>
2014-10-09 13:34:03 +0200

    Removed append_error function.

--- jb_common/utils.py
+++ jb_common/utils.py
@@ -140,34 +140,6 @@ def get_really_full_name(user):
     return user.get_full_name() or unicode(user)
 
 
-def append_error(form, error_message, fieldname="__all__"):
-    """This function is called if a validation error is found in form data
-    which cannot be found by the ``is_valid`` method itself.  The reason is
-    very simple: For many types of invalid data, you must take other forms in
-    the same view into account.
-
-    See, for example, `split_after_deposition.is_referentially_valid`.
-
-    FixMe: Possibly this function will be superseeded by
-    <https://code.djangoproject.com/ticket/20867>.
-
-    :Parameters:
-      - `form`: the form to which the erroneous field belongs
-      - `error_message`: the message to be presented to the user
-      - `fieldname`: the name of the field that triggered the validation
-        error.  It is optional, and if not given, the error is considered an
-        error of the form as a whole.
-
-    :type form: ``forms.Form`` or ``forms.ModelForm``.
-    :type fieldname: str
-    :type error_message: unicode
-    """
-    # FixMe: Is it really a good idea to call ``is_valid`` here?
-    # ``append_error`` is also called in ``clean`` methods after all.
-    form.is_valid()
-    form._errors.setdefault(fieldname, ErrorList()).append(error_message)
-
-
 dangerous_markup_pattern = re.compile(r"([^\\]|\A)!\[|[\n\r](-+|=+)\s*$")
 def check_markdown(text):
     """Checks whether the Markdown input by the user contains only permitted
