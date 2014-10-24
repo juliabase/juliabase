e6a2076e4ff31c6b27ffe70aaa3b5dd437b9dba7
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-10-24 10:56:44 +0200

    Added copyright headers to all migrations.

--- kicker/migrations/0001_initial.py
+++ kicker/migrations/0001_initial.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#
+# This file is part of JuliaBase, the samples database.
+#
+# Copyright © 2008–2014 Forschungszentrum Jülich, Germany,
+#                       Marvin Goblet <m.goblet@fz-juelich.de>,
+#                       Torsten Bronger <t.bronger@fz-juelich.de>
+#
+# You must not use, install, pass on, offer, sell, analyse, modify, or
+# distribute this software without explicit permission of the copyright holder.
+# If you have received a copy of this software without the explicit permission
+# of the copyright holder, you must destroy it immediately and completely.
+
+from __future__ import absolute_import, unicode_literals, division
 
 from django.db import models, migrations
 from django.conf import settings
