0bb36298ef9ce008cb648bfa4026218462b1a335
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-10-30 19:47:55 +0100

    Moved JSON view add_samples from "samples" to "jb_institute".

--- samples/urls.py
+++ samples/urls.py
@@ -134,7 +134,6 @@ urlpatterns = patterns("samples.views",
                        (r"^latest_split/(?P<sample_name>.+)", "split_and_rename.latest_split"),
                        (r"^login_remote_client$", "json_client.login_remote_client"),
                        (r"^logout_remote_client$", "json_client.logout_remote_client"),
-                       (r"^add_sample$", "json_client.add_sample"),
                        (r"^add_alias$", "json_client.add_alias"),
                        (r"^change_my_samples$", "json_client.change_my_samples"),
 
