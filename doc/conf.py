ad12da470eeab72e5c5cb23e4f7b9c6fe8a37940
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-11-04 09:52:00 +0100

    Improved web page layout and logos.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ sys.path.append(os.path.abspath('..'))
 extensions = ["sphinx.ext.autodoc"]#, "sphinx.ext.mathjax"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
@@ -119,7 +119,7 @@ html_theme = 'sphinxdoc'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "images/juliabase.png"
+# html_logo = "images/juliabase.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -129,7 +129,7 @@ html_favicon = "images/juliabase.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["images"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
