07b45020dac942d2cccf318230b9b5829f701750
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-11-10 10:02:05 +0100

    Adjusted all paths in the WSGI file to the docs' project structure.

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,12 @@
 import os
 import sys
 
-sys.path.append("/home/bronger/src/jb_institute")
+sys.path.append("/home/username/myproject")
+sys.path.append("/home/username/myproject/juliabase")
 sys.stdout = sys.stderr
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-os.environ['MPLCONFIGDIR'] = '/home/bronger/.config/matplotlib'
+os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+os.environ["MPLCONFIGDIR"] = "/home/username/.config/matplotlib"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
