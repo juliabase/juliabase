8dc1981100de50abe745b53edb175b84ac0d07fe
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-11-18 16:15:02 +0100

    Changed five chamber "crawler" to add the samples to Rosalee.

--- remote_client/examples/five_chamber.py
+++ remote_client/examples/five_chamber.py
@@ -14,35 +14,74 @@
 
 from __future__ import unicode_literals
 
-import sys, os, datetime, glob
+import sys, os, datetime, random
 sys.path.append(os.path.abspath(".."))
 from jb_remote_institute import *
 
+
 login("juliabase", "12345")
 
-timestamp = datetime.datetime.now()
-try:
-    sample_id = get_sample("14-EM-1")
-except SampleNotFound as exception:
-    sample = exception.sample
-    sample.currently_responsible_person = "r.calvert"
-    sample.topic = "Legacy"
-    sample_id = sample.submit()
+def create_depo(timestamp, sample_name, comments=None):
+    try:
+        sample_id = get_sample(sample_name)
+    except SampleNotFound as exception:
+        sample = exception.sample
+        sample.currently_responsible_person = "r.calvert"
+        sample.current_location = "Rosalee's office"
+        sample.topic = "Legacy"
+        sample_id = sample.submit()
 
-    substrate = Substrate()
-    substrate.timestamp =  - datetime.timedelta(minutes=1)
-    substrate.timestamp_inaccuracy = 3
-    substrate.sample_ids = [sample_id]
-    substrate.material = "corning"
-    substrate.operator = "r.calvert"
-    substrate.submit()
+        substrate = Substrate()
+        substrate.timestamp = timestamp - datetime.timedelta(minutes=1)
+        substrate.timestamp_inaccuracy = 3
+        substrate.sample_ids = [sample_id]
+        substrate.material = "corning"
+        substrate.operator = "r.calvert"
+        substrate.submit()
+    else:
+        sample = Sample(id_=sample_id)
+    sample.add_to_my_samples("r.calvert")
 
-deposition = FiveChamberDeposition()
-deposition.sample_ids = [sample_id]
-layer = FiveChamberLayer(deposition)
-layer.date = datetime.date.today()
-layer.chamber = "p"
+    deposition = FiveChamberDeposition()
+    deposition.sample_ids = [sample_id]
+    deposition.operator = "r.calvert"
+    deposition.timestamp = timestamp
+    deposition.comments = comments
 
-deposition.submit()
+    layer = FiveChamberLayer(deposition)
+    layer.date = timestamp.date()
+    layer.temperature_1 = int(random.uniform(150, 160))
+    layer.temperature_2 = int(random.uniform(150, 180))
+    layer.sih4 = int(random.uniform(0, 5))
+    layer.h2 = int(random.uniform(0, 2))
+    layer.chamber = "p"
+    layer.layer_type = "p"
+
+    layer = FiveChamberLayer(deposition)
+    layer.date = timestamp.date()
+    layer.temperature_1 = int(random.uniform(100, 160))
+    layer.temperature_2 = int(random.uniform(170, 180))
+    layer.sih4 = int(random.uniform(0, 4))
+    layer.h2 = int(random.uniform(0, 1))
+    layer.chamber = "i2"
+    layer.layer_type = "i"
+
+    layer = FiveChamberLayer(deposition)
+    layer.date = timestamp.date()
+    layer.temperature_1 = int(random.uniform(130, 150))
+    layer.temperature_2 = int(random.uniform(155, 160))
+    layer.sih4 = int(random.uniform(0, 10))
+    layer.h2 = int(random.uniform(0, 12))
+    layer.chamber = "n"
+    layer.layer_type = "n"
+
+    deposition.submit()
+
+create_depo(datetime.datetime(2014, 11, 1, 10, 30), "14S-001")
+create_depo(datetime.datetime(2014, 11, 2, 11, 10), "14S-002")
+create_depo(datetime.datetime(2014, 11, 2, 12, 10), "14S-003","AFTER: chamber cleaned!")
+create_depo(datetime.datetime(2014, 11, 2, 13, 10), "14S-004")
+create_depo(datetime.datetime(2014, 11, 2, 14, 10), "14S-005")
+create_depo(datetime.datetime(2014, 11, 2, 15, 10), "14S-006")
 
 logout()
