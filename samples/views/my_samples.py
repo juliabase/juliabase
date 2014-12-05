juliabase/juliabase

commit b0cac9dbe55883a19051eeef6e92651fede7f50d

Fixed an key error in my_samples view.

samples/views/my_samples.py
@@ -155,7 +155,7 @@ def is_referentially_valid(current_user, my_samples_form, action_form):
                 action_form.add_error("clearance", _("If you copy samples over to another person who cannot fully view one of the "
                                             "samples, you must select a clearance option."))
                 referentially_valid = False
-        if action_data["clearance"] is not None:
+        if "clearance" in action_data:
             failed_samples = []
             for sample in my_samples_form.cleaned_data["samples"]:
                 try:

commit c04360280b929a2afb37e8f72718cf07118fc729

Made removal of samples from "My Samples" possible again.
This fixes a regression introduced in b0cac9dbe55883a19051eeef6e92651fede7f50d.

samples/views/my_samples.py
@@ -154,7 +154,7 @@ def is_referentially_valid(current_user, my_samples_form, action_form):
                 action_form.add_error("clearance", _("If you copy samples over to another person who cannot fully view one of the "
                                             "samples, you must select a clearance option."))
                 referentially_valid = False
-        if "clearance" in action_data:
+        if action_data.get("clearance") is not None:
             failed_samples = []
             for sample in my_samples_form.cleaned_data["samples"]:
                 try: