7666f152070ace31cef63ce7ff9b42389cb9bce2
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-12-09 11:10:13 +0100

    Made tests running again.

--- inm/tests/test_json_export.py
+++ inm/tests/test_json_export.py
@@ -21,13 +21,29 @@ from django.test.client import Client
 import samples.views.shared_utils
 
 
-substrate_data = {"sample IDs": [1, 2], "timestamp inaccuracy": 0, "timestamp": "2010-12-02T11:07:36",
-                  "material": "corning", "external operator": None, "finished": True, "comments": "",
-                  "operator": "testuser", "type": "substrate", "ID": 1}
+class JsonTestCase(TestCase):
 
-class ExportTest(TestCase):
+    def remove_dynamic_fields(self, dictionary):
+        for key, value in list(dictionary.items()):
+            if key == "last_modified":
+                del dictionary[key]
+            elif isinstance(value, dict):
+                self.remove_dynamic_fields(value)
+
+    def assertJsonDictEqual(self, response, dictionary):
+        data = json.loads(response.content.decode("ascii"))
+        self.remove_dynamic_fields(data)
+        self.assertEqual(data, dictionary)
+        
+
+substrate_data = {"samples": [1, 2], "timestamp_inaccuracy": 0, "timestamp": "2010-12-02T11:07:36",
+                  "material": "corning", "external_operator": None, "finished": True, "comments": "",
+                  "operator": "testuser", "content_type": "substrate", "id": 1}
+
+class ExportTest(JsonTestCase):
     fixtures = ["test_inm"]
     urls = "inm.tests.urls"
+    maxDiff = None
 
     def setUp(self):
         self.client = Client()
@@ -36,33 +52,30 @@ class ExportTest(TestCase):
     def test_substrate_export(self):
         response = self.client.get("/processes/1", HTTP_ACCEPT="application/json")
         self.assertEqual(response["Content-Type"], "application/json")
-        self.assertEqual(json.loads(response.content.decode("ascii")), substrate_data)
+        self.assertJsonDictEqual(response, substrate_data)
 
     def test_pds_measurement_export(self):
         response = self.client.get("/pds_measurements/1", HTTP_ACCEPT="application/json")
-        self.assertEqual(response["Content-Type"], "application/json")
-        self.assertEqual(
-            json.loads(response.content.decode("ascii")),
-            {"sample IDs": [2], "apparatus": "pds1", "timestamp inaccuracy": 0, "timestamp": "2010-12-02T12:07:36",
-             "external operator": None,
-             "operator": "testuser", "finished": True, "comments": "",
-             "PDS number": 1, "type": "PDS measurement", "raw data file": "T:/Daten/pds/p4600-/pd4636.dat"})
+        self.assertEqual(response["content-type"], "application/json")
+        self.assertJsonDictEqual(response,
+            {"samples": [2], "apparatus": "pds1", "timestamp_inaccuracy": 0, "timestamp": "2010-12-02T12:07:36",
+             "external_operator": None,
+             "operator": "testuser", "finished": True, "comments": "", "id": 2,
+             "number": 1, "content_type": "PDS measurement", "raw_datafile": "T:/Daten/pds/p4600-/pd4636.dat"})
 
     def test_sample_export(self):
         response = self.client.get("/samples/by_id/2", HTTP_ACCEPT="application/json")
-        self.assertEqual(response["Content-Type"], "application/json")
-        self.assertEqual(
-            json.loads(response.content.decode("ascii")),
-            {"currently responsible person": "testuser", "name": "10-TB-second", "tags": "", "topic": None, "purpose": "",
-             "current location": u"Torsten's office", "split origin": None, "ID": 2,
-             "process #1": {"sample IDs": [1, 2], "type": "substrate", "timestamp inaccuracy": 0,
-                            "timestamp": "2010-12-02T11:07:36", "external operator": None, "operator": "testuser", "finished": True,
-                            "comments": "",
-                            "material": "corning", "ID": 1},
-             "process #2": {"sample IDs": [2], "type": "PDS measurement", "timestamp inaccuracy": 0,
-                            "timestamp": "2010-12-02T12:07:36", "external operator": None, "operator": "testuser", "finished": True,
-                            "comments": "",
-                            "apparatus": "pds1", "PDS number": 1, "raw data file": "T:/Daten/pds/p4600-/pd4636.dat"}
+        self.assertEqual(response["content-type"], "application/json")
+        self.assertJsonDictEqual(response,
+            {"currently_responsible_person": "testuser", "name": "10-TB-second", "tags": "", "topic": None, "purpose": "",
+             "current_location": u"Torsten's office", "split_origin": None, "id": 2,
+             "process #1": {"samples": [1, 2], "content_type": "substrate", "timestamp_inaccuracy": 0,
+                            "timestamp": "2010-12-02T11:07:36", "external_operator": None, "operator": "testuser",
+                            "finished": True, "comments": "", "material": "corning", "id": 1},
+             "process #2": {"samples": [2], "content_type": "PDS measurement", "timestamp_inaccuracy": 0,
+                            "timestamp": "2010-12-02T12:07:36", "external_operator": None, "operator": "testuser",
+                            "finished": True, "comments": "", "apparatus": "pds1", "number": 1, "id": 2,
+                            "raw_datafile": "T:/Daten/pds/p4600-/pd4636.dat"}
             })
 
 
@@ -73,7 +86,7 @@ class SharedUtilsTest(TestCase):
         self.assertEqual(samples.views.shared_utils.capitalize_first_letter("ärgerlich"), "Ärgerlich")
 
 
-class AdminExportTest(TestCase):
+class AdminExportTest(JsonTestCase):
     fixtures = ["test_inm"]
     urls = "inm.tests.urls"
 
@@ -83,8 +96,8 @@ class AdminExportTest(TestCase):
 
     def test_substrate_by_sample_export(self):
         response = self.client.get("/substrates_by_sample/1", HTTP_ACCEPT="application/json")
-        self.assertEqual(response["Content-Type"], "application/json")
-        self.assertEqual(json.loads(response.content.decode("ascii")),
+        self.assertEqual(response["content-type"], "application/json")
+        self.assertJsonDictEqual(response,
                          {"operator": "testuser", "timestamp": "2010-12-02T11:07:36", "material": "corning",
-                          "timestamp inaccuracy": 0, "comments": "", "finished": True, "sample IDs": [1, 2],
-                          "external operator": None, "type": "substrate", "ID": 1})
+                          "timestamp_inaccuracy": 0, "comments": "", "finished": True, "samples": [1, 2],
+                          "external_operator": None, "content_type": "substrate", "id": 1})
