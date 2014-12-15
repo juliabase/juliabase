3865f56c58b0d6c40ad5ea1eefe13a15bcddb7bc
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-12-15 11:41:01 +0100

    Added and used PatternGenerator.
    It is only a test implementation yet.

--- inm/urls.py
+++ inm/urls.py
@@ -29,69 +29,73 @@ root URL module.
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url, patterns
+from samples.views.shared_utils import camel_case_to_underscores
 
 
-urlpatterns = patterns("samples.views.lab_notebook",
-                        url(r"^5-chamber_depositions/lab_notebook/(?P<year_and_month>.*)/export/",
-                            "export", {"process_name": "FiveChamberDeposition"}, "export_lab_notebook_FiveChamberDeposition"),
-                        url(r"^5-chamber_depositions/lab_notebook/(?P<year_and_month>.*)",
-                            "show", {"process_name": "FiveChamberDeposition"}, "lab_notebook_FiveChamberDeposition"),
-                        )
+class PatternGenerator(object):
 
-urlpatterns += patterns("inm.views.samples",
-                        (r"^samples/add/$", "sample.add"),
-                        (r"^samples/(?P<sample_name>.+)-/copy_informal_stack/$", "sample.copy_informal_stack"),
+    def __init__(self, url_patterns, views_prefix):
+        self.views_prefix = views_prefix + "."
+        self.url_patterns = url_patterns
 
-                        url(r"^cluster_tool_depositions/add/$", "cluster_tool_deposition.edit",
-                            {"number": None}, "add_cluster_tool_deposition"),
-                        url(r"^cluster_tool_depositions/(?P<number>.+)/edit/$",
-                            "cluster_tool_deposition.edit", name="edit_cluster_tool_deposition"),
-                        (r"^cluster_tool_depositions/(?P<number>.+)",
-                         "cluster_tool_deposition.show"),
+    def physical_process(self, class_name, identifying_field=None, url_name=None, views={"add", "edit"}):
+        camel_case_class_name = camel_case_to_underscores(class_name)
+        url_name = url_name or camel_case_class_name + "s"
+        assert not views - {"add", "edit", "show", "lab_notebook"}
+        if not identifying_field:
+            identifying_field = camel_case_class_name + "_id"
+        if "add" in views:
+            self.url_patterns.append(url(r"^{}/add/$".format(url_name), self.views_prefix + camel_case_class_name + ".edit",
+                                         {identifying_field: None}, "add_" + camel_case_class_name))
+        if "edit" in views:
+            self.url_patterns.append(url(r"^{}/(?P<{}>.+)/edit/$".format(url_name, identifying_field),
+                                         self.views_prefix + camel_case_class_name + ".edit", name="edit_" +
+                                         camel_case_class_name))
+        if "show" in views:
+            self.url_patterns.append(url(r"^{}/(?P<{}>.+)".format(url_name, identifying_field),
+                                         self.views_prefix + camel_case_class_name + ".show", name="show_" +
+                                         camel_case_class_name))
+        if "lab_notebook" in views:
+            self.url_patterns.extend([url(r"^{}/lab_notebook/(?P<year_and_month>.*)/export/".format(url_name),
+                                          "samples.views.lab_notebook.export", {"process_name": class_name},
+                                          "export_lab_notebook_" + camel_case_class_name),
+                                      url(r"^{}/lab_notebook/(?P<year_and_month>.*)".format(url_name),
+                                          "samples.views.lab_notebook.show", {"process_name": class_name},
+                                          "lab_notebook_" + camel_case_class_name)])
 
-                        url(r"^pds_measurements/add/$", "pds_measurement.edit", {"number": None}, "add_pds_measurement"),
-                        url(r"^pds_measurements/(?P<number>\d+)/edit/$", "pds_measurement.edit",
-                            name="edit_pds_measurement"),
-                        (r"^pds_measurements/(?P<number>.+)", "pds_measurement.show"),
 
-                        url(r"^5-chamber_depositions/add/$", "five_chamber_deposition.edit",
-                            {"number": None}, "add_five_chamber_deposition"),
-                        url(r"^5-chamber_depositions/(?P<number>.+)/edit/$",
-                            "five_chamber_deposition.edit", name="edit_five_chamber_deposition"),
-                        (r"^5-chamber_depositions/(?P<number>.+)", "five_chamber_deposition.show"),
+urlpatterns = []
 
-                        url(r"^substrates/(?P<substrate_id>.+)/edit/$", "substrate.edit",
-                            name="edit_substrate"),
+pattern_generator = PatternGenerator(urlpatterns, "inm.views.samples")
+pattern_generator.physical_process("cluster_tool_deposition", "number")
+pattern_generator.physical_process("five_chamber_deposition", "number", "5-chamber_depositions",
+                                   {"add", "edit", "lab_notebook"})
+pattern_generator.physical_process("pds_measurement", "number")
+pattern_generator.physical_process("substrate", views={"edit"})
+pattern_generator.physical_process("structuring", views={"edit"})
+pattern_generator.physical_process("solarsimulator_measurement")
 
-                        url(r"^solarsimulator_measurements/add/$", "solarsimulator_measurement.edit",
-                            {"solarsimulator_measurement_id": None}, "add_solarsimulator_measurement"),
-                        url(r"^solarsimulator_measurements/(?P<solarsimulator_measurement_id>\d+)/edit/$",
-                            "solarsimulator_measurement.edit", name="edit_solarsimulator_measurement"),
-                        url(r"solarsimulator_measurements/(?P<solarsimulator_measurement_id>\d+)$",
-                            "solarsimulator_measurement.show", name="show_solarsimulator_measurement"),
 
-                        url(r"^structuring_process/add/$", "structuring.edit", {"solarsimulator_measurement_id": None},
-                            "add_sructuring"),
-                        url(r"^structuring_process/(?P<structuring_id>.+)/edit/$", "structuring.edit",
-                            name="edit_structuring"),
+urlpatterns += [
+    url(r"^samples/add/$", "inm.views.samples.sample.add"),
+    url(r"^samples/(?P<sample_name>.+)-/copy_informal_stack/$", "inm.views.samples.sample.copy_informal_stack"),
 
-                        (r"^add_sample$", "json_client.add_sample"),
-                        (r"^substrates_by_sample/(?P<sample_id>.+)", "json_client.substrate_by_sample"),
-                        (r"^next_deposition_number/(?P<letter>.+)", "json_client.next_deposition_number"),
-                        (r"^solarsimulator_measurements/by_filepath", "json_client.get_maike_by_filepath"),
-                        (r"^structurings/by_sample/(?P<sample_id>.+)", "json_client.get_current_structuring"),
-                        (r"^solarsimulator_measurements/matching/(?P<irradiation>[A-Za-z0-9.]+)/(?P<sample_id>\d+)/"
-                         r"(?P<cell_position>[^/]+)/(?P<date>\d{4}-\d\d-\d\d)/",
-                         "json_client.get_matching_solarsimulator_measurement"),
+    url(r"^add_sample$", "inm.views.samples.json_client.add_sample"),
+    url(r"^substrates_by_sample/(?P<sample_id>.+)", "inm.views.samples.json_client.substrate_by_sample"),
+    url(r"^next_deposition_number/(?P<letter>.+)", "inm.views.samples.json_client.next_deposition_number"),
+    url(r"^solarsimulator_measurements/by_filepath", "inm.views.samples.json_client.get_maike_by_filepath"),
+    url(r"^structurings/by_sample/(?P<sample_id>.+)", "inm.views.samples.json_client.get_current_structuring"),
+    url(r"^solarsimulator_measurements/matching/(?P<irradiation>[A-Za-z0-9.]+)/(?P<sample_id>\d+)/"
+        r"(?P<cell_position>[^/]+)/(?P<date>\d{4}-\d\d-\d\d)/",
+        "inm.views.samples.json_client.get_matching_solarsimulator_measurement"),
 
-                        (r"^claims/(?P<username>.+)/add_oldstyle/$", "claim.add_oldstyle"),
+    url(r"^claims/(?P<username>.+)/add_oldstyle/$", "inm.views.samples.claim.add_oldstyle"),
 
-                        url(r"^stacks/(?P<sample_id>\d+)", "stack.show_stack", {"thumbnail": False}, "stack_diagram"),
-                        url(r"^stacks/thumbnails/(?P<sample_id>\d+)", "stack.show_stack", {"thumbnail": True},
-                            "stack_diagram_thumbnail"),
+    url(r"^stacks/(?P<sample_id>\d+)", "inm.views.samples.stack.show_stack", {"thumbnail": False}, "stack_diagram"),
+    url(r"^stacks/thumbnails/(?P<sample_id>\d+)", "inm.views.samples.stack.show_stack", {"thumbnail": True},
+        "stack_diagram_thumbnail"),
 
-                        url(r"layouts/(?P<sample_id>\d+)/(?P<process_id>\d+)", "layout.show_layout"),
+    url(r"layouts/(?P<sample_id>\d+)/(?P<process_id>\d+)", "inm.views.samples.layout.show_layout"),
 
-                        (r"^printer_label/(?P<sample_id>\d+)$", "sample.printer_label"),
-
-                        )
+    url(r"^printer_label/(?P<sample_id>\d+)$", "inm.views.samples.sample.printer_label"),
+]
