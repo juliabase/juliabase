a2274e60a4497e16c2dcfadf0f3a2b821ad36727
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-12-16 08:38:51 +0100

    Set default_role in the Sphinx configuration.

--- doc/conf.py
+++ doc/conf.py
@@ -83,7 +83,7 @@ release = '1.0'
 exclude_trees = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'py:obj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
