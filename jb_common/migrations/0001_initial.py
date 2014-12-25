1c563fe10ced11cb369677dba4f4a8c9754e9ba2
Torsten Bronger <bronger@physik.rwth-aachen.de>
2014-12-25 19:09:13 +0100

    Squashed migrations of jb_common.

--- jb_common/migrations/0001_initial.py
+++ jb_common/migrations/0001_initial.py
@@ -31,6 +31,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=30, verbose_name='name')),
+                ('app_label', models.CharField(max_length=30, verbose_name='app label')),
             ],
             options={
                 'ordering': ['name'],
