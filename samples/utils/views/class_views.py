902d8e11c01bbf2c78cd7e1f27eb974d3601df1e
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-01-26 14:19:40 +0100

    Added missing import.

--- samples/utils/views/class_views.py
+++ samples/utils/views/class_views.py
@@ -26,7 +26,7 @@ from django.utils.safestring import mark_safe
 from django.utils.encoding import force_text
 import django.forms as forms
 from django.forms.util import ValidationError
-from jb_common.utils.base import camel_case_to_underscores, is_json_requested, format_enumeration
+from jb_common.utils.base import camel_case_to_underscores, is_json_requested, format_enumeration, int_or_zero
 from samples import permissions
 from . import forms as utils
 from .feed import Reporter
