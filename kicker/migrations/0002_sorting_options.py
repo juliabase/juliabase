ae873263668b14cfac676e0a7860679ca9270009
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-03-11 04:56:15 +0100

    Added migration with new kicker model sorting options.

---
+++ kicker/migrations/0002_sorting_options.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kicker', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='kickernumber',
+            options={'ordering': ['timestamp'], 'get_latest_by': 'timestamp', 'verbose_name': 'kicker number', 'verbose_name_plural': 'kicker numbers'},
+        ),
+        migrations.AlterModelOptions(
+            name='match',
+            options={'ordering': ['timestamp'], 'get_latest_by': 'timestamp', 'verbose_name': 'match', 'verbose_name_plural': 'matches'},
+        ),
+        migrations.AlterModelOptions(
+            name='shares',
+            options={'ordering': ['timestamp'], 'get_latest_by': 'timestamp', 'verbose_name': 'shares', 'verbose_name_plural': 'shareses'},
+        ),
+        migrations.AlterModelOptions(
+            name='stockvalue',
+            options={'ordering': ['timestamp'], 'get_latest_by': 'timestamp', 'verbose_name': 'stock value', 'verbose_name_plural': 'stock values'},
+        ),
+    ]
