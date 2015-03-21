8b238f4775ddef813a9ab2f3a53021ca5b76f7b3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-03-21 23:14:18 +0100

    Added migrations that implement changes necessary due to the upgrade to Django 1.8.

---
+++ samples/migrations/0003_django_1_8_changes.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('samples', '0002_userdetails_my_samples_list_timestamp'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='clearance',
+            name='last_modified',
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified'),
+        ),
+        migrations.AlterField(
+            model_name='externaloperator',
+            name='alternative_email',
+            field=models.EmailField(max_length=254, verbose_name='alternative email', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='externaloperator',
+            name='email',
+            field=models.EmailField(max_length=254, verbose_name='email'),
+        ),
+        migrations.AlterField(
+            model_name='process',
+            name='last_modified',
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified'),
+        ),
+        migrations.AlterField(
+            model_name='sample',
+            name='last_modified',
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified'),
+        ),
+        migrations.AlterField(
+            model_name='sampleseries',
+            name='last_modified',
+            field=models.DateTimeField(auto_now=True, verbose_name='last modified'),
+        ),
+        migrations.AlterField(
+            model_name='task',
+            name='last_modified',
+            field=models.DateTimeField(help_text='YYYY-MM-DD HH:MM:SS', verbose_name='last modified', auto_now=True),
+        ),
+        migrations.AlterField(
+            model_name='task',
+            name='operator',
+            field=models.ForeignKey(related_name='operated_tasks', verbose_name='operator', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+    ]
