2a82ec69e0131f1e4d9479eb090f903143f45718
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-04-18 13:44:56 +0200

    Added migration with non-essential changes for "samples".

---
+++ samples/migrations/0004_user_details.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('samples', '0003_django_1_8_changes'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userdetails',
+            name='my_layers',
+            field=models.TextField(help_text='in JSON format', verbose_name='My Layers', blank=True),
+        ),
+    ]
