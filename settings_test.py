3a574de5fe60a2a1f7a63fea4014f368ab30ff59
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-11-05 20:18:31 +0100

    Switched to Sqlite for test runner.

--- settings_test.py
+++ settings_test.py
@@ -32,3 +32,6 @@ from settings import *
 PASSWORD_HASHERS = (
     "django.contrib.auth.hashers.MD5PasswordHasher",
 )
+
+DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
+DATABASES["default"]["TEST"] = {"NAME": "/tmp/juliabase-test-db"}
