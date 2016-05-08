118c31d783f281db4927d4ef6a9a31a09206347e
Torsten Bronger <bronger@physik.rwth-aachen.de>
2016-05-08 17:28:09 +0200

    Removed debug code.

--- samples/models/common.py
+++ samples/models/common.py
@@ -719,7 +719,7 @@ class PhysicalProcess(Process):
     def delete(self, *args, **kwargs):
         dry_run = kwargs.get("dry_run", False)
         if dry_run:
-            if self.timestamp < django.utils.timezone.now() - datetime.timedelta(hours=1) and False:
+            if self.timestamp < django.utils.timezone.now() - datetime.timedelta(hours=1):
                 description = _("You are not allowed to delete the process “{process}” because it is older than "
                                 "one hour.").format(process=self)
                 raise samples.permissions.PermissionError(user, description)
