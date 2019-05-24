d1b129b7a84a03f1b54c2c5bc9a65081e8da5955
Torsten Bronger <bronger@physik.rwth-aachen.de>
2019-05-24 13:58:05 +0200

    Disabled OAI-PMH until authentication measures are implemented.

--- urls.py
+++ urls.py
@@ -41,7 +41,7 @@ import oai_pmh.urls, institute.urls, jb_common.urls, samples.urls
 
 
 urlpatterns = [
-    url(r"^oai-pmh", include(oai_pmh.urls)),
+#    url(r"^oai-pmh", include(oai_pmh.urls)),
     url(r"", include(institute.urls)),
     url(r"", include(jb_common.urls)),
     url(r"", include(samples.urls)),
