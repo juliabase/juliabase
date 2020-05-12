4c0ca2dc0fba34c9083c912b5d391d39f509f0c0
Torsten Bronger <bronger@physik.rwth-aachen.de>
2020-05-12 14:58:54 +0200

    Removed special password hasher setting for tests.
    It broke the tests with Django 2.0.13 for me.

--- settings_test.py
+++ settings_test.py
@@ -27,9 +27,6 @@
 from settings import *
 
 
-PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.MD5PasswordHasher",
-]
 
 DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
 DATABASES["default"]["TEST"] = {"NAME": "/tmp/juliabase-test-db"}
