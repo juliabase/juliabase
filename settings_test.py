7c6ecdab43c0921db4d53832098c859c8f61c531
Torsten Bronger <bronger@physik.rwth-aachen.de>
2021-10-15 02:59:09 +0200

    Fix DB configuration for test SQLite DB

--- settings_test.py
+++ settings_test.py
@@ -26,7 +26,7 @@ from settings import *
 
 os.environ["CRAWLERS_DATA_DIR"] = "/tmp"
 
-DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
-DATABASES["default"]["TEST"] = {"NAME": "/tmp/juliabase-test-db"}
+DATABASES["default"] = {"ENGINE": "django.db.backends.sqlite3",
+                        "NAME": "/tmp/juliabase-test-db"}
 
 RDF_ROOT_URL = "https://inm.example.com"
