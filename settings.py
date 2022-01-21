dc82900ce56ba1b7b7160f53f96f7cfd1c769975
Torsten Bronger <bronger@physik.rwth-aachen.de>
2022-01-21 15:29:12 +0100

    Adapt Redis settings

--- settings.py
+++ settings.py
@@ -159,12 +159,9 @@ LANGUAGES = [("en", _("English")), ("de", _("German"))]
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
-        "TIMEOUT": 3600 * 24 * 28,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "TIMEOUT": 3600 * 24 * 28
     }
 }
 if DEBUG:
