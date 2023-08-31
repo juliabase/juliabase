a7ea9a3be2cff3de29b5a7e3d5cefac2c4206daa
Torsten Bronger <bronger@physik.rwth-aachen.de>
2023-08-31 17:30:22 +0200

    Add Dart boilerplate

---
+++ dart/main.dart
@@ -0,0 +1,13 @@
+library okuda_ui;
+
+import "dart:html";
+
+void main() {
+
+  // FixMe: We don’t need onLoad once
+  // https://bugs.chromium.org/p/chromium/issues/detail?id=874749 is finally
+  // fixed.
+  window.onLoad.listen((_) {
+    print(window.location.href);
+  });
+}
