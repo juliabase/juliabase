92461d7b840e621fd931b264f8d18d70fc0efb49
Torsten Bronger <bronger@physik.rwth-aachen.de>
2023-09-01 16:21:16 +0200

    Add rudimentary adding of attachments to result process
    This is only about the DOM manipulation, and even this is incomplete.

--- dart/main.dart
+++ dart/main.dart
@@ -1,13 +1,16 @@
 library juliabase;
 
 import "dart:html";
+part "result.dart";
 
 void main() {
-
   // FixMe: We don’t need onLoad once
   // https://bugs.chromium.org/p/chromium/issues/detail?id=874749 is finally
   // fixed.
   window.onLoad.listen((_) {
-    print(window.location.href);
+    if (window.location.pathname == "/results/add/") {
+      querySelector("#add-attachment")!
+          .addEventListener("pointerdown", resultAddAttachment);
+    }
   });
 }
---
+++ dart/result.dart
@@ -0,0 +1,36 @@
+part of juliabase;
+
+void resultAddAttachment(Event event) {
+  final attachments = querySelector("#attachments")!;
+  var index = 0;
+  for (var c in attachments.children) {
+    final i = int.parse(c.getAttribute("id")!.split("_")[0]);
+    if (i > index) {
+      index = i;
+    }
+  }
+  index++;
+  final newAttach = document.createElement("tbody");
+  newAttach.setAttribute("id", "${index}_attachment");
+  newAttach.setAttribute("style", "border: 2px solid");
+  final row = document.createElement("tr");
+  newAttach.append(row);
+  final firstCol = document.createElement("td");
+  firstCol.setAttribute("class", "field-label");
+  firstCol.setAttribute("style", "padding: 2ex");
+  final secondCol = document.createElement("td");
+  secondCol.setAttribute("class", "field-input");
+  secondCol.setAttribute("style", "padding: 2ex");
+  row.append(firstCol);
+  row.append(secondCol);
+  final label = document.createElement("label");
+  label.setAttribute("for", "${index}_id_image_file");
+  label.text = "Image file:";
+  firstCol.append(label);
+  final input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.setAttribute("name", "${index}_image_file");
+  input.setAttribute("id", "${index}_id_image_file");
+  secondCol.append(input);
+  attachments.append(newAttach);
+}
