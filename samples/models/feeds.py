e14929f59fb068b69c726c0db06d0bae90805eeb
Torsten Bronger <bronger@physik.rwth-aachen.de>
2023-10-05 12:42:51 +0200

    Correct docstring

--- samples/models/feeds.py
+++ samples/models/feeds.py
@@ -95,14 +95,11 @@ class FeedEntry(PolymorphicModel):
         raise NotImplementedError
 
     def get_additional_template_context(self, user):
-        """Return a dictionary with additional context that should be available in the
-        template.  It is similar to
-        :py:meth:`institute.models.FiveChamberDeposition.get_additional_template_context`.
-        However, in contrast to this other method, the feed version is
-        implemented in the abstract base class, so it is defined in all feed
-        models.  The rationale for this is that it is used in almost every feed
-        model anyway.  If not overridden, this method returns an empty
-        dictionary.
+        """Return a dictionary with additional context that should be available
+        in the template.  It is implemented in the abstract base class, so it
+        is defined in all feed models.  The rationale for this is that it is
+        used in almost every feed model anyway.  If not overridden, this method
+        returns an empty dictionary.
 
         :param user: the user fetching the feed
 
