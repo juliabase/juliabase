f4a045356d8d293fdcfd6567933999f330a1b4dd
Torsten Bronger <bronger@physik.rwth-aachen.de>
2024-04-12 16:24:15 +0200

    Remove DCAT that is not available in rdflib any more.

--- samples/ontology_symbols.py
+++ samples/ontology_symbols.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from rdflib.namespace import Namespace, DCAT, FOAF, OWL, PROV, RDF, RDFS
+from rdflib.namespace import Namespace, FOAF, OWL, PROV, RDF, RDFS
 from rdflib import URIRef, BNode, Literal
 
 
