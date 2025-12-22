a518e6d3fac7cb3ddf8d73d604e3c3c8a9620ec0
Ayoub <a.lamrani@fz-juelich.de>
2025-12-22 13:24:35 +0100

    [JULIABASECLEANING] Moved Datatables code from juliabase to iek5

--- jb_common/static/juliabase/js/lab_notebook.js
+++ jb_common/static/juliabase/js/lab_notebook.js
@@ -16,19 +16,44 @@ $(document).ready( function () {
             sortDescending: gettext(": activate to sort column descending")
         }
     };
-
+console.log("lab_notebook.js loaded");
     
     // We use a timer here to allow everything to load before calling datatables.
     // This might not be necessary if 
     setTimeout(function() {
-        $('.lab-notebook').DataTable({
-                order: [],
-                // pageLength: 50,
-                scrollX: true,
-                scrollY: '50vh',
-                language: langDict,
-            fixedColumns: true,
-            fixedHeader: true // Enable FixedHeader extension
+        // We use this to initialize datatables for all the tables of class "lab-notebook"
+        // EXCEPT the ones with class "manual-datatable".
+        $('.lab-notebook:not(.manual-datatable)').DataTable({
+            order: [],
+            // pageLength: 50,
+            scrollX: true,
+            scrollY: '50vh',
+            language: langDict,
+        fixedColumns: true,
+        fixedHeader: true // Enable FixedHeader extension
         }); // Initialize the DataTables for the rest of the tables with the class "lab-notebook"
     }, 1000);
+    
+    try{
+        if(no_bs5 == false){
+            
+        }
+    } catch(error){
+        // Use jQuery to select the specific div and find its child table element
+        var $table = $('.lock-header').find('table');
+    
+        // Add new classes to the table element
+        $table.addClass('lab-notebook table table table-hover table-bordered table-striped-columns');
+
+        // Preserve existing classes by ensuring they are not already present
+        var existingClasses = $table.attr('class').split(' ');
+        existingClasses.forEach(function(className) {
+            if (!$table.hasClass(className)) {
+                $table.addClass(className);
+            }
+        });
+
+        // Find the thead element within the table and add new classes to it
+        $table.find('thead').addClass('align-middle');
+    }
   } );
--- static/js/lab_notebook.js
+++ static/js/lab_notebook.js
@@ -1,50 +1,188 @@
 $(document).ready(function () {
-    // Function to sort the table
-    function sortTable(columnIndex) {
-        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-        table = document.querySelector('.lab-notebook');
-        switching = true;
-        // Set the sorting direction to ascending
-        dir = "asc";
-
-        while (switching) {
-            switching = false;
-            rows = table.rows;
-
-            for (i = 1; i < (rows.length - 1); i++) {
-                shouldSwitch = false;
-                x = rows[i].getElementsByTagName("td")[columnIndex];
-                y = rows[i + 1].getElementsByTagName("td")[columnIndex];
-
-                if (dir === "asc") {
-                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                        shouldSwitch = true;
-                        break;
-                    }
-                } else if (dir === "desc") {
-                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                        shouldSwitch = true;
-                        break;
-                    }
-                }
-            }
-
-            if (shouldSwitch) {
-                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                switching = true;
-                switchcount++;
-            } else {
-                if (switchcount === 0 && dir === "asc") {
-                    dir = "desc";
-                    switching = true;
-                }
-            }
+    // Add manual-datatable class to tables that should not be auto-initialized by juliabase
+    var manualTables = [
+        '#screenprinter',
+        '#five-chamber',
+        '#six-chamber',
+        '#cluster-tool-2',
+        '#cluster-tool-1',
+        '#dip-bench',
+        '#hercules',
+        '#jana',
+        '#joseph',
+        '#lada',
+        '#large-sputter',
+        '#large-area',
+        '#maria',
+        '#p-hot-wire',
+        '#wetbench',
+        '#screenprinter-paste',
+        '#screenprinter-screen',
+        '#experiment'
+    ];
+    $(manualTables.join(', ')).addClass('manual-datatable');
+    console.log("Manual datatable classes addesdddd.");
+    // Define langDict for DataTables (copied from juliabase)
+    var langDict = {
+        processing:     gettext("Processing..."),
+        search:         gettext("Search&nbsp;:"),
+        lengthMenu:     gettext("Show _MENU_ entries"),
+        info:           gettext("Showing _START_ to _END_ of total _TOTAL_ entries"),
+        infoEmpty:      gettext("Showing 0 to 0 of total 0 entries"),
+        infoFiltered:   gettext("(filtered from total _MAX_ entries)"),
+        infoPostFix:    "",
+        loadingRecords: gettext("Loading records..."),
+        zeroRecords:    gettext("No entries found"),
+        emptyTable:     gettext("No data available"),
+        aria: {
+            sortAscending:  gettext(": activate to sort column ascending"),
+            sortDescending: gettext(": activate to sort column descending")
         }
-    }
+    };
 
-    // Event listener for column headers
-    $(".lab-notebook th").click(function () {
-        var columnIndex = $(this).index();
-        sortTable(columnIndex);
-    });
+    // Specific initializations moved from juliabase
+    $('#experiment').DataTable({
+            order: [],
+            scrollX: true,
+            scrollY: '50vh',
+            fixedColumns: true,
+            fixedHeader: true,
+            columnDefs: [
+                {
+                    targets: '_all', // Apply to all columns except the first one
+                    render: function (data, type, row, meta) {
+                        // Ignore rendering for the first column
+                        // if (meta.col === 0) return data;
+        
+                        if (type === 'display') {
+                            // Create a temporary element to parse the HTML
+                            var div = document.createElement('div');
+                            div.innerHTML = data;
+        
+                            // Check if the content contains a link
+                            var link = div.querySelector('a');
+                            if (link) {
+                                // Extract the URL and the visible text
+                                var url = link.href;
+                                var text = link.textContent || link.innerText || "";
+        
+                                // Truncate the visible text if it's too long
+                                if (text.length > 30) {
+                                    text = text.substr(0, 30) + '...';
+                                }
+        
+                                // Rebuild the anchor tag with the original URL and truncated text
+                                return `<a href="${url}" title="${link.textContent}">${text}</a>`;
+                            } else {
+                                // If no link, just truncate the text
+                                var text = div.textContent || div.innerText || "";
+                                if (text.length > 30) {
+                                    text = text.substr(0, 30) + '...';
+                                }
+                                return `<div title="${div.textContent}">${text}</div>`;
+                            }
+                        }
+                        return data;
+                    }
+                }
+            ],
+        });
+        
+
+        // Custom table declaration for the table with the id "screenprinter-paste" 
+        $('#screenprinter-paste').DataTable({
+
+            createdRow: function (row, data, dataIndex){
+                  // FIXME: This is quite a terrible way to pick a column, since new columns can be added
+                  // and the current setting will break if not fixed. Use column name instead of number
+                  if(data[11] === "True"){
+                    // You wanna know why I am using the "table-danger" class instead
+                    // of simply changing the color of the row directly?
+                    // Well, Bootstrap loves overriding everything. Might aswell use
+                    // its classes instead. 
+                    $(row).addClass('table-danger');
+                  }
+              },
+                order: [],
+                // pageLength: 50,
+                scrollX: true,
+                scrollY: '50vh',
+                language: langDict,
+                fixedColumns: true,
+                fixedHeader: true // Enable FixedHeader extension
+                }); // Initialize the DataTables for the rest of the tables with the class "lab-notebook"
+                  
+  
+        $('#screenprinter-screen').DataTable({
+  
+            createdRow: function (row, data, dataIndex){
+                  // FIXME: This is quite a terrible way to pick a column, since new columns can be added
+                  // and the current setting will break if not fixed. Use column name instead of number
+                  if(data[15] === ""){
+                    // You wanna know why I am using the "table-danger" class instead
+                    // of simply changing the color of the row directly?
+                    // Well, Bootstrap loves overriding everything. Might aswell use
+                    // its classes instead. 
+                    $(row).addClass('table-danger');
+                  }
+              },
+                order: [],
+                // pageLength: 50,
+                scrollY: '50vh',
+                scrollX: true,
+                language: langDict,
+                fixedColumns: true,
+                fixedHeader: true // Enable FixedHeader extension
+                }); // Initialize the DataTables for the rest of the tables with the class "lab-notebook" 
+      
+
+        $('#screenprinter').DataTable( {
+            language: langDict,
+            order: [],
+            // pageLength: 50,
+            scrollX: true,
+            scrollY: '50vh',
+            fixedColumns: true,
+            fixedHeader: true, // Enable FixedHeader extension
+            columnDefs: [ {
+                targets: [ 2, 4, 6 ], // Assuming the column index is 0
+                render: function (data, type, row) {
+                    if (data === "None") {
+                        return "<i>None</i>"; // Convert "None" to null for proper sorting
+                    }
+                    // Extract numeric part of the string after 'S'
+                    // FIXME: Temporary solution, since this regex pattern does not 
+                    // take into account strings that start with a lower case
+                    var matches = data.match(/\d+/i);
+                    // That is why we turn the first character of all 
+                    // the strings to upper case
+                    data = data.charAt(0).toUpperCase() + data.slice(1);
+
+                    // Check if there are strings that match the pattern
+                    if (matches !== null) {
+                        // If yes, take the number
+                        var num = matches[0];
+                        // If the number has only one digit, pad a zero at the beginning
+                        // to sort things properly (i.e. S7 becomes S07) 
+                        if (num.length === 1) {
+                            let extracted = data.substring(0, data.indexOf(num));
+                            num = '0' + num; // pad a zero at the beginning
+                            // Check if there isn't anything attached to the number
+                            if (/^\d+$/.test(data.charAt(-1)) === false){
+                                // If yes attach back all the things the number starts and ends with
+                                return extracted + num + data.slice(num.length);
+                            }
+                            // If not just return the beginning of the string and number
+                            return extracted + num;
+                        }
+                        // If the number has more than one digit, you can simply return it
+                        return data;
+                    } else {
+                        return data; // return 0 if no number is found
+                    }
+                },
+                type: 'numeric'
+            } ]
+        } ); 
+
 });
